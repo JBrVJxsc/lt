5cb1607986ea07e794d78698c8942dbf2c34edeb
Xu ZHANG <laonawuli@126.com>
2016-05-18 08:52:23 -0700

    [44. Wildcard Matching][Accepted]

---
+++ 44-Wildcard-Matching/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0;
+        int j = 0;
+        int star = -1;
+        int mark = -1;
+        while (i < s.length()) {
+            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                ++i;
+                ++j;
+            } else if (j < p.length() && p.charAt(j) == '*') {
+                star = j;
+                j++;
+                mark = i;
+                //这一步是关键，匹配s中当前字符与p中‘＊’后面的字符，如果匹配，则在第一个if中处理，如果不匹配，则继续比较s中的下一个字符。
+            } else if (star != -1) {
+                j = star + 1;
+                i = ++mark;
+            } else {
+                return false;
+            }
+        }
+        //最后在此处处理多余的‘＊’，因为多个‘＊’和一个‘＊’意义相同。
+        while (j < p.length() && p.charAt(j) == '*') {
+            ++j;
+        }
+        return j == p.length();
+    }
+}
