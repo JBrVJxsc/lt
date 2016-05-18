c61e97d67cab67edeb7da592ee27dd02176e8275
Xu ZHANG <laonawuli@126.com>
2016-05-17 22:56:03 -0700

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -8,7 +8,7 @@ public class Solution {
             return s.length() == 0;
         }
         
-        if (p.length() == 1) {
+        if (p.length() == 1 || p.charAt(1) != '*') {
             if (s.length() == 0) {
                 return false;
             }
@@ -18,26 +18,16 @@ public class Solution {
             return isMatch(s.substring(1), p.substring(1));
         }
         
-        if (p.charAt(1) == '*') {
-            if (isMatch(s, p.substring(2))) {
+        if (isMatch(s, p.substring(2))) {
+            return true;
+        }
+        int i = 0;
+        while (i < s.length() && (s.charAt(i) == p.charAt(0) || p.charAt(0) == '.')) {
+            if (isMatch(s.substring(i + 1), p.substring(2))) {
                 return true;
             }
-            int i = 0;
-            while (i < s.length() && (s.charAt(i) == p.charAt(0) || p.charAt(0) == '.')) {
-                if (isMatch(s.substring(i + 1), p.substring(2))) {
-                    return true;
-                }
-                i++;
-            }
-            return false;
-        } else {
-            if (s.length() == 0) {
-                return false;
-            }            
-            if (s.charAt(0) != p.charAt(0) && p.charAt(0) != '.') {
-                return false;
-            }
-            return isMatch(s.substring(1), p.substring(1));
+            i++;
         }
+        return false;
     }
 }
