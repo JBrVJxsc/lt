a19d6b4325e6eeece1bd1aafa4f6d9dfa3074f60
Xu ZHANG <laonawuli@126.com>
2016-05-19 09:07:53 -0700

    [90. Subsets II][Accepted]

---
+++ 90-Subsets-II/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("nums cannot be null.");
+        }
+        
+        List<List<Integer>> lists = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        
+        for (int i : nums) {
+            List<List<Integer>> temp = new ArrayList<>();
+            for (List<Integer> list : lists) {
+                List<Integer> l = new ArrayList<>(list);
+                l.add(i);
+                if (!set.contains(l)) {
+                    temp.add(l);
+                    set.add(l);
+                }
+            }
+            List<Integer> l = new ArrayList<>();
+            l.add(i);
+            if (!set.contains(l)) {
+                temp.add(l);
+                set.add(l);
+            }
+            lists.addAll(temp);
+        }
+        
+        lists.add(new ArrayList<>());
+        return lists;
+    }
+}
