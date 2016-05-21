60f0206908c3fc1f4cb2f939cf56497cb091fcd3
Xu ZHANG <laonawuli@126.com>
2016-05-20 23:06:39 -0700

    [91. Decode Ways][Accepted]

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,29 +1,27 @@
 public class Solution {
     public int numDecodings(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException("s cannot be null.");
-        }
         
-        if (s.length() == 0) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
         
         int[] dp = new int[s.length()];
-        for (int i = s.length() - 1; 0 <= i; i--) {
-            if (s.charAt(i) == '0') {
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == '0') {
                 continue;
             }
-            // dp[i] = dp[i + 1] + dp[i + 2].
             if (i == s.length() - 1) {
                 dp[i] = 1;
             } else {
                 dp[i] += dp[i + 1];
-                if (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7') {
+                if (c == '1' || c == '2' && s.charAt(i + 1) < '7') {
                     if (i == s.length() - 2) {
                         dp[i] += 1;
                     } else {
-                        dp[i] += dp[i + 2];                 
-                    }                    
+                        dp[i] += dp[i + 2];
+                    }
                 }
             }
         }
