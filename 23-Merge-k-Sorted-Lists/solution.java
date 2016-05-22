ec543f5dc5e520e492e7bfbc5cec10e4923dcf24
Xu ZHANG <laonawuli@126.com>
2016-05-21 20:46:58 -0700

    [23. Merge k Sorted Lists][Accepted]

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -8,50 +8,29 @@
  */
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        return merge(lists, 0, lists.length - 1);
-    }
-    
-    private ListNode merge(ListNode[] lists, int start, int end) {
-        if (start > end) {
-            return null;
-        }
-        if (start == end) {
-            return lists[start];
-        }
-        int mid = start + (end - start) / 2;
-        ListNode a = merge(lists, start, mid);
-        ListNode b = merge(lists, mid + 1, end);
-        return merge(a, b);
-    }
-    
-    private ListNode merge(ListNode a, ListNode b) {
-        if (a == null) {
-            return b;
-        }
-        if (b == null) {
-            return a;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode n1, ListNode n2) {
+                return Integer.compare(n1.val, n2.val);
+            }
+        });
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
         }
         ListNode dummy = new ListNode(-1);
         ListNode next = dummy;
-        
-        while (a != null || b != null) {
-            if (a == null) {
-                next.next = b;
-                b = b.next;
-            } else if (b == null) {
-                next.next = a;
-                a = a.next;
-            } else if (a.val > b.val) {
-                next.next = b;
-                b = b.next;
-            } else {
-                next.next = a;
-                a = a.next;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.remove();
+            next.next = node;
+            node = node.next;
+            if (node != null) {
+                pq.add(node);
             }
             next.next.next = null;
             next = next.next;
         }
-        
         return dummy.next;
     }
 }
