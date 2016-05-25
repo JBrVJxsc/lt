bd5f21ee7c1d21f9cbc0d0944dded06c48c806ba
Xu ZHANG <laonawuli@126.com>
2016-05-25 00:27:42 -0700

    [17. Letter Combinations of a Phone Number][Accepted]

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,31 +1,33 @@
 public class Solution {
-    
-    private String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    
-    public List<String> letterCombinations(String digits) {
-        List<String> list = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
-            return list;
-        }
-        for (char c : digits.toCharArray()) {
-            list = combine(list, map[c - '0']);
-        }
-        return list;
+  
+  private String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};  
+  
+  public List<String> letterCombinations(String digits) {
+    List<String> list = new ArrayList<>();
+    if (digits == null || digits.length() == 0) {
+      return list;
     }
     
-    private List<String> combine(List<String> list, String s) {
-        List<String> result = new ArrayList<>();
-        if (list.size() == 0) {
-            for (char c : s.toCharArray()) {
-                result.add(String.valueOf(c));
-            }
-        } else {
-            for (char c : s.toCharArray()) {
-                for (String str : list) {
-                    result.add(str + String.valueOf(c));
-                }
-            }            
-        }
-        return result;
+    for (char c : digits.toCharArray()) {
+      list = combine(list, map[c - '0']);
     }
+    
+    return list;
+  }
+
+  private List<String> combine(List<String> list, String s) {
+    List<String> result = new ArrayList<>();
+    if (list.size() == 0) {
+      for (char c : s.toCharArray()) {
+        result.add(String.valueOf(c));
+      }
+    } else {
+      for (char c : s.toCharArray()) {
+        for (String str : list) {
+          result.add(str + String.valueOf(c));            
+        }
+      }      
+    }
+    return result;
+  }
 }
