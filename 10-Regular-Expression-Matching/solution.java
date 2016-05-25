e1662c08e4ebb33c97e8173d12e320ddd8a95f0a
Xu ZHANG <laonawuli@126.com>
2016-05-24 23:55:22 -0700

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,34 +1,37 @@
 public class Solution {
-    public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
-        }
-        
-        if (p.length() == 0) {
-            return s.length() == 0;
-        }
-        
-        if (p.length() == 1 || p.charAt(1) != '*') {
-            if (s.length() == 0) {
-                return false;
-            }
-            if (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') {
-                return isMatch(s.substring(1), p.substring(1));
-            }
-            return false;
-        }
-        
-        if (isMatch(s, p.substring(2))) {
-            return true;
-        }
-        
-        int i = 0;
-        while (i < s.length() && (s.charAt(i) == p.charAt(0) || p.charAt(0) == '.')) {
-            if (isMatch(s.substring(i + 1), p.substring(2))) {
-                return true;
-            }
-            i++;
-        }
-        return false;
+  public boolean isMatch(String s, String p) {
+    if (s == null || p == null) {
+      throw new IllegalArgumentException("s and p cannot be null.");
     }
+    
+    if (p.length() == 0) {
+      return s.length() == 0;
+    }
+    
+    if (p.length() == 1 || p.charAt(1) != '*') {
+      if (s.length() == 0) {
+        return false;
+      }
+      
+      if (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') {
+        return isMatch(s.substring(1), p.substring(1));
+      }
+      
+      return false;
+    }
+    
+    if (p.charAt(1) == '*') {
+      if (isMatch(s, p.substring(2))) {
+        return true;
+      }
+      int i = 0;
+      while (i < s.length() && (s.charAt(i) == p.charAt(0) || p.charAt(0) == '.')) {
+        if (isMatch(s.substring(i + 1), p.substring(2))) {
+          return true;
+        }
+        i++;
+      }
+    }
+    return false;
+  }
 }
