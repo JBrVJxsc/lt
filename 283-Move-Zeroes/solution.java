d88234ae3648655b73eec2de1999195771fcebf5
Xu ZHANG <laonawuli@126.com>
2016-05-24 22:50:38 -0700

    [283. Move Zeroes][Accepted]

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,13 +1,14 @@
 public class Solution {
-    public void moveZeroes(int[] nums) {
-        int cur = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[cur++] = nums[i];
-            }
-        }
-        for (; cur < nums.length; cur++) {
-            nums[cur] = 0;
-        }
+  public void moveZeroes(int[] nums) {
+    int cur = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[cur++] = nums[i];
+      }
     }
+    
+    for (int i = cur; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
 }
