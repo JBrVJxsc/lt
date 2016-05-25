4ff5ed78dc4e0b2dd4e7d8e936b7185dcc1d0e02
Xu ZHANG <laonawuli@126.com>
2016-05-25 00:36:19 -0700

    [56. Merge Intervals][Accepted]

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -1,37 +1,26 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 public class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval i1, Interval i2) {
-                return Integer.compare(i1.start, i2.start);
-            }
-        });
-        List<Interval> result = new ArrayList<>();
-        
-        for (Interval i : intervals) {
-            if (result.size() == 0) {
-                result.add(i);
-            } else {
-                Interval last = result.get(result.size() - 1);
-                if (i.start > last.end) {
-                    result.add(i);
-                } else if (i.end <= last.end) {
-                    continue;
-                } else {
-                    last.end = i.end;
-                }
-            }
+  public List<Interval> merge(List<Interval> intervals) {
+    List<Interval> list = new ArrayList<>();
+    
+    Collections.sort(intervals, (a, b) -> {
+      return Integer.compare(a.start, b.start);
+    });
+    
+    for (Interval i : intervals) {
+      if (list.size() == 0) {
+        list.add(i);
+      } else {
+        Interval last = list.get(list.size() - 1);
+        if (i.end <= last.end) {
+          continue;
+        } else if (i.start <= last.end) {
+          last.end = i.end;
+        } else {
+          list.add(i);
         }
-        
-        return result;
+      }
     }
+    
+    return list;
+  }
 }
