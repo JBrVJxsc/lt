51bb9a5607dae9fcb129f9ee50616effe997e3d8
Xu ZHANG <laonawuli@126.com>
2016-05-25 00:02:33 -0700

    [23. Merge k Sorted Lists][Accepted]

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -1,36 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode n1, ListNode n2) {
-                return Integer.compare(n1.val, n2.val);
-            }
-        });
-        for (ListNode node : lists) {
-            if (node != null) {
-                pq.add(node);
-            }
+  public ListNode mergeKLists(ListNode[] lists) {
+      if (lists.length == 0) {
+          return null;
+      }
+    Queue<ListNode> pq = new PriorityQueue<>((a, b) -> {
+      return Integer.compare(a.val, b.val);
+    });
+    for (ListNode node : lists) {
+        if (node != null) {
+      pq.add(node);
         }
-        ListNode dummy = new ListNode(-1);
-        ListNode next = dummy;
-        while (!pq.isEmpty()) {
-            ListNode node = pq.remove();
-            next.next = node;
-            node = node.next;
-            if (node != null) {
-                pq.add(node);
-            }
-            next.next.next = null;
-            next = next.next;
-        }
-        return dummy.next;
     }
+    
+    ListNode dummy = new ListNode(-1);
+    ListNode next = dummy;
+    
+    while (!pq.isEmpty()) {
+      ListNode node = pq.remove();
+      next.next = node;
+      node = node.next;
+      if (node != null) {
+        pq.add(node);
+      }
+      next.next.next = null;
+      next = next.next;
+    }
+    
+    return dummy.next;
+  }
 }
