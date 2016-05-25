dd7364b0663698c4a57f67ee6455fa316173b832
Xu ZHANG <laonawuli@126.com>
2016-05-24 23:28:27 -0700

    [278. First Bad Version][Accepted]

--- 278-First-Bad-Version/solution.java
+++ 278-First-Bad-Version/solution.java
@@ -1,23 +1,20 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        return binary(1, n);
+  public int firstBadVersion(int n) {
+    return binary(1, n);
+  }
+  
+  private int binary(int l, int r) {
+    int m = l + (r - l) / 2;
+    boolean isBad = isBadVersion(m);
+    if (isBad) {
+      isBad = isBadVersion(m - 1);
+      if (!isBad) {
+        return m;
+      } else {
+        return binary(l, m - 1);
+      }
+    } else {
+      return binary(m + 1, r);
     }
-    
-    private int binary(int left, int right) {
-        int mid = left + (right - left) / 2;
-        boolean isBad = isBadVersion(mid);
-        if (!isBad) {
-            return binary(mid + 1, right);
-        } else {
-            isBad = isBadVersion(mid - 1);
-            if (isBad) {
-                return binary(left, mid - 1);
-            } else {
-                return mid;
-            }
-        }
-    }
+  }
 }
