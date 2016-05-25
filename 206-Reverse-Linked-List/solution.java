ba6fd99fc5ffc119b9b14d521965b8c5bad17416
Xu ZHANG <laonawuli@126.com>
2016-05-25 00:30:49 -0700

    [206. Reverse Linked List][Accepted]

--- 206-Reverse-Linked-List/solution.java
+++ 206-Reverse-Linked-List/solution.java
@@ -1,27 +1,19 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        
-        while (head.next != null) {
-            ListNode next = head.next;
-            head.next = head.next.next;
-            next.next = dummy.next;
-            dummy.next = next;
-        }
-        
-        return dummy.next;
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+    
+    while (head.next != null) {
+      ListNode next = head.next;
+      head.next = head.next.next;
+      next.next = dummy.next;
+      dummy.next = next;
+    }
+    
+    return dummy.next;
+  }
 }
