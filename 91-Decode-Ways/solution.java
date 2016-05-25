97f48c8d30856870aacdfa1e5e1cfad4c6c39ee8
Xu ZHANG <laonawuli@126.com>
2016-05-24 23:21:08 -0700

    [91. Decode Ways][Accepted]

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,31 +1,31 @@
 public class Solution {
-    public int numDecodings(String s) {
-        
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        
-        int[] dp = new int[s.length()];
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char c = s.charAt(i);
-            if (c == '0') {
-                continue;
-            }
-            if (i == s.length() - 1) {
-                dp[i] = 1;
-            } else {
-                dp[i] += dp[i + 1];
-                if (c == '1' || c == '2' && s.charAt(i + 1) < '7') {
-                    if (i == s.length() - 2) {
-                        dp[i] += 1;
-                    } else {
-                        dp[i] += dp[i + 2];
-                    }
-                }
-            }
-        }
-        
-        return dp[0];
+  public int numDecodings(String s) {
+    if (s == null || s.length() == 0) {
+      return 0;
     }
+    
+    int[] dp = new int[s.length()];
+    
+    for (int i = s.length() - 1; 0 <= i; i--) {
+      char c = s.charAt(i);
+      if (c == '0') {
+        continue;
+      }
+      
+      if (i == s.length() - 1) {
+        dp[i] += 1;
+      } else {
+        dp[i] += dp[i + 1];
+        if (c == '1' || c == '2' && s.charAt(i + 1) < '7') {
+          if (i == s.length() - 2) {
+            dp[i] += 1;
+          } else {
+            dp[i] += dp[i + 2];
+          }
+        }
+      }
+    }
+    
+    return dp[0];
+  }
 }
