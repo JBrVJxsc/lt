be4bc0db16b1543dec4bc5b37500f18bc0c1b901
Xu ZHANG <laonawuli@126.com>
2016-05-24 21:43:57 -0700

    [76. Minimum Window Substring][Accepted]

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,55 +1,53 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null) {
-            throw new IllegalArgumentException("s and t cannot be null.");
+          if (s == null || t == null) {
+    throw new IllegalArgumentException("s and t cannot be null.");
+  }
+  
+  Map<Character, Integer> map = new HashMap<>();
+  for (char c : t.toCharArray()) {
+    Integer num = map.getOrDefault(c, 0);
+    map.put(c, num + 1);
+  }
+  
+  Map<Character, Integer> found = new HashMap<>();  
+  String result = "";
+  int l = 0;
+  int r = 0;
+  int counter = 0;
+  int curLen = Integer.MAX_VALUE;
+
+  while (r < s.length()) {
+    while (r < s.length() && counter < t.length()) {
+      char c = s.charAt(r);
+      if (map.get(c) != null) {
+        Integer num = found.getOrDefault(c, 0);
+        if (num < map.get(c)) {
+          counter++;
         }
-        
-        Map<Character, Integer> target = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            Integer n = target.getOrDefault(c, 0);
-            target.put(c, n + 1);
+        found.put(c, num + 1);
+      }
+      r++;
+    }
+    
+    while (counter == t.length()) {
+      char c = s.charAt(l);
+      if (map.get(c) != null) {
+        Integer num = found.get(c);
+        if (num <= map.get(c)) {
+          String str = s.substring(l, r);
+          if (str.length() < curLen) {
+            curLen = str.length();
+            result = str;
+          }
+          counter--;
         }
-        
-        Map<Character, Integer> found = new HashMap<>();
-        int curL = 0;
-        int curR = 0;
-        int count = 0;
-        int curLen = Integer.MAX_VALUE;
-        String result = "";
-        
-        while (curR < s.length()) {
-            while (curR < s.length() && count < t.length()) {
-                char c = s.charAt(curR);
-                Integer n = target.get(c);
-                if (n != null) {
-                    Integer f = found.getOrDefault(c, 0);
-                    if (f < n) {
-                        count++;
-                    }
-                    found.put(c, f + 1);                    
-                }
-                curR++;
-            }
-            
-            while (count == t.length()) {
-                char c = s.charAt(curL);
-                Integer n = target.get(c);
-                if (n != null) {
-                    Integer f = found.getOrDefault(c, 0);
-                    if (f <= n) {
-                        String str = s.substring(curL, curR);
-                        if (str.length() < curLen) {
-                            curLen = str.length();
-                            result = str;
-                        }
-                        count--;                        
-                    }
-                    found.put(c, f - 1);
-                }
-                curL++;
-            }
-        }
-        
-        return result;
+        found.put(c, num - 1);           
+      }
+      l++;
+    }
+  }
+  
+  return result;
     }
 }
