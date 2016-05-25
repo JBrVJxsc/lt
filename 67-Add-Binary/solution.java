879d60e93f0fb1c5192ecbcde3c299d8cfdc8b90
Xu ZHANG <laonawuli@126.com>
2016-05-24 23:35:10 -0700

    [67. Add Binary][Accepted]

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,30 +1,28 @@
 public class Solution {
-    public String addBinary(String a, String b) {
-        if (a == null) {
-            return b;
-        }
-        if (b == null) {
-            return a;
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        int pA = a.length() - 1;
-        int pB = b.length() - 1;
-        int remain = 0;
-        
-        while (pA >= 0 || pB >= 0) {
-            int numA = pA < 0 ? 0 : a.charAt(pA--) - '0';
-            int numB = pB < 0 ? 0 : b.charAt(pB--) - '0';
-            remain += numA + numB;
-            sb.insert(0, remain % 2);
-            remain /= 2;
-        }
-        
-        if (remain == 1) {
-            sb.insert(0, 1);
-        }
-        
-        return sb.toString();
+  public String addBinary(String a, String b) {
+    if (a == null) {
+      return b;
     }
+    if (b == null) {
+      return a;
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    int remain = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    
+    while (i >= 0 || j >= 0) {
+      remain += i >= 0 ? a.charAt(i--) - '0' : 0;
+      remain += j >= 0 ? b.charAt(j--) - '0' : 0;
+      sb.insert(0, remain % 2);
+      remain /= 2;
+    }
+    
+    if (remain == 1) {
+      sb.insert(0, 1);
+    }
+    
+    return sb.toString();
+  }
 }
