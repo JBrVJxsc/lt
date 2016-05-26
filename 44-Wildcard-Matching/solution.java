de8735d9056177f1308ee2504eef5369084bac4f
Xu ZHANG <laonawuli@126.com>
2016-05-25 23:42:17 -0700

    [44. Wildcard Matching][Accepted]

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,30 +1,30 @@
 public class Solution {
-    public boolean isMatch(String s, String p) {
-        int i = 0;
-        int j = 0;
-        int star = -1;
-        int match = 0;
-        while (i < s.length()) {
-            if (j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
-                i++;
-                j++;
-            } else if (j < p.length() && p.charAt(j) == '*') {
-                star = j;
-                j++;
-                match = i;
-            } else if (star != -1) {
-                match++;
-                i = match;
-                j = star + 1;
-            } else {
-                return false;
-            }
-        }
-        
-        while (j < p.length() && p.charAt(j) == '*') {
-            j++;
-        }
-        
-        return j == p.length();
+  public boolean isMatch(String s, String p) {
+    int i = 0;
+    int j = 0;
+    int star = -1;
+    int match = 0;
+    while (i < s.length()) {
+      if (i < s.length() && j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
+        i++;
+        j++;
+      } else if (j < p.length() && p.charAt(j) == '*') {
+        star = j;
+        j++;
+        match = i;
+      } else if (star != -1) {
+        match++;
+        i = match;
+        j = star + 1;
+      } else {
+        return false;
+      }
     }
+    
+    while (j < p.length() && p.charAt(j) == '*') {
+      j++;
+    }
+    
+    return j == p.length();
+  }
 }
