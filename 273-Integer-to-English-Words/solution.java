17b7cd80ae3421961f63ed5813c24afad9217678
Xu ZHANG <laonawuli@126.com>
2016-05-26 09:24:30 -0700

    [273. Integer to English Words][Accepted]

--- 273-Integer-to-English-Words/solution.java
+++ 273-Integer-to-English-Words/solution.java
@@ -1,33 +1,30 @@
 public class Solution {
-    
-    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};    
-    
-    public String numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-        return helper(num);
+  private String[] digits = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+  private String[] lessThan20 = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private String[] lessThan100 = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+  
+  public String numberToWords(int num) {
+    if (num == 0) {
+      return "Zero";
     }
-    
-    private String helper(int n) {
-        String str = "";
-        if (n < 10) {
-            str = belowTen[n];
-        } else if (n < 20) {
-            str = belowTwenty[n - 10];
-        } else if (n < 100) {
-            str = belowHundred[n / 10] + " " + helper(n % 10);
-        } else if (n < 1000) {
-            str = helper(n / 100) + " Hundred " + helper(n % 100);
-        } else if (n < 1000000) {
-            str = helper(n / 1000) + " Thousand " + helper(n % 1000);
-        } else if (n < 1000000000) {
-            str = helper(n / 1000000) + " Million " + helper(n % 1000000);
-        } else {
-            str = helper(n / 1000000000) + " Billion " + helper(n % 1000000000);
-        }
-        return str.trim();
-    }
+    return convert(num);
+  }
+
+  private String convert(int n) {
+    String str = "";
+    if (n < 10) {
+      str = digits[n];
+    } else if (n < 20) {
+      str = lessThan20[n - 10];
+    } else if (n < 100) {
+      str = lessThan100[n / 10] + " " + digits[n % 10];
+    } else if (n < 1000) {
+      str = convert(n / 100) + " Hundred " + convert(n % 100);
+    } else if (n < 1000000) {
+      str = convert(n / 1000) + " Thousand " + convert(n % 1000);
+    } else if (n < 1000000000) {
+      str = convert(n / 1000000) + " Million " + convert(n % 1000000);
+    } else str = convert(n / 1000000000) + " Billion " + convert(n % 1000000000);
+    return str.trim();
+  }
 }
