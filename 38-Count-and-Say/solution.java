886574391d2ed4d9de0cb2ffe5e4d3a8a6a3db91
Xu ZHANG <laonawuli@126.com>
2016-05-25 22:55:29 -0700

    [38. Count and Say][Accepted]

--- 38-Count-and-Say/solution.java
+++ 38-Count-and-Say/solution.java
@@ -1,33 +1,33 @@
 public class Solution {
-    public String countAndSay(int n) {
-        String str = "1";
-        if (n == 1) {
-            return str;
-        }
-        
-        for (int i = 1; i < n; i++) {
-            str = countAndSay(str);
-        }
-        
-        return str;
+  public String countAndSay(int n) {
+    String s = "1";
+    if (n == 0) {
+      return "";
     }
     
-    private String countAndSay(String str) {
-        StringBuilder sb = new StringBuilder();
-        int count = 1;
-        char cur = str.charAt(0);
-        for (int i = 1; i < str.length(); i++) {
-            if (cur == str.charAt(i)) {
-                count++;
-            } else {
-                sb.append(count);
-                sb.append(cur);
-                count = 1;
-                cur = str.charAt(i);
-            }
-        }
-        sb.append(count);
-        sb.append(cur);        
-        return sb.toString();
+    for (int i = 1; i < n; i++) {
+      s = countAndSay(s);
     }
+    
+    return s;
+  }
+
+  private String countAndSay(String s) {
+    StringBuilder sb = new StringBuilder();
+    char cur = s.charAt(0);
+    int len = 1;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == cur) {
+        len++;
+      } else {
+        sb.append(len);
+        sb.append(cur);
+        cur = s.charAt(i);
+        len = 1;
+      }
+    }
+    sb.append(len);
+    sb.append(cur);
+    return sb.toString();
+  }
 }
