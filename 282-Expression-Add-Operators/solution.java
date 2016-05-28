2e78ec23e896628fa92220eacbad5b5991c00182
Xu ZHANG <laonawuli@126.com>
2016-05-27 19:46:25 -0700

    [282. Expression Add Operators][Accepted]

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,32 +1,31 @@
-public class Solution {
-    public List<String> addOperators(String num, int target) {
-        List<String> list = new ArrayList<>();
-        if (num == null || num.length() == 0) {
+    public class Solution {
+        public List<String> addOperators(String num, int target) {
+            List<String> list = new ArrayList<>();
+            dfs(num, target, list, 0, "", 0, 0);
             return list;
         }
-        dfs(num, target, list, 0, 0, 0, "");
-        return list;
-    }
-    
-    private void dfs(String num, int target, List<String> list, int pos, long val, long last, String cur) {
-        if (pos == num.length()) {
-            if (val == target) {
-                list.add(cur);
-            }
-        } else {
-            for (int i = pos; i < num.length(); i++) {
-                if (i != pos && num.charAt(pos) == '0') {
-                    break;
+
+        private void dfs(String num, int target, List<String> list, int index, String curStr, long curVal, long last) {
+            if (index == num.length()) {
+                if (curVal == target) {
+                    list.add(curStr);
                 }
-                long n = Long.parseLong(num.substring(pos, i + 1));
-                if (pos == 0) {
-                    dfs(num, target, list, i + 1, n, n, cur + n);
+                return;
+            }
+
+            for (int i = index; i < num.length(); i++) {
+                if (i != index && num.charAt(index) == '0') {
+                    return;
+                }
+                String sub = num.substring(index, i + 1);
+                long val = Long.valueOf(sub);
+                if (index == 0) {
+                    dfs(num, target, list, i + 1, sub, val, val);
                 } else {
-                    dfs(num, target, list, i + 1, val + n, n, cur + "+" + n);
-                    dfs(num, target, list, i + 1, val - n, -n, cur + "-" + n);
-                    dfs(num, target, list, i + 1, val - last + last * n, last * n, cur + "*" + n);
+                    dfs(num, target, list, i + 1, curStr + "+" + sub, curVal + val, val);
+                    dfs(num, target, list, i + 1, curStr + "-" + sub, curVal - val, -val);
+                    dfs(num, target, list, i + 1, curStr + "*" + sub, curVal - last + last * val, last * val);
                 }
             }
         }
     }
-}
