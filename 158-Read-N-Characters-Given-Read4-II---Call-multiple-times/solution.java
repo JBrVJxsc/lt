de638fe49da75ebaf56aa26eee776f4d64747e3a
Xu ZHANG <laonawuli@126.com>
2016-05-28 14:08:12 -0700

    [158. Read N Characters Given Read4 II - Call multiple times][Accepted]

--- 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
+++ 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
@@ -2,46 +2,44 @@
       int read4(char[] buf); */
 
 public class Solution extends Reader4 {
-    /**
+  /**
      * @param buf Destination buffer
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
-    private Queue<Character> q = new LinkedList<>();
-     
-    public int read(char[] buf, int n) {
-        int total = 0;
-        int p = 0;
-        char[] temp = new char[4];
-        
-        while (n > 0) {
-            if (q.size() < 4) {
-                int num = read4(temp);
-                for (int i = 0; i < num; i++) {
-                    q.add(temp[i]);
-                }
-            }
-            
-            int num = 0;
-            while (!q.isEmpty() && num < 4 && num < n) {
-                temp[num++] = q.remove();
-            }
-
-            if (num > n) {
-                num = n;
-            }
-            total += num;
-            n -= num;
-            
-            for (int i = 0; i < num; i++) {
-                buf[p++] = temp[i];
-            }
-            
-            if (num < 4) {
-                break;
-            }
+  private Queue<Character> queue = new LinkedList<>();
+  
+  public int read(char[] buf, int n) {
+    char[] temp = new char[4];
+    int count = 0;
+    int pos = 0;
+    
+    while (n > 0) {
+      if (queue.size() < 4) {
+        int num = read4(temp);
+        for (int i = 0; i < num; i++) {
+          queue.add(temp[i]);
         }
-        
-        return total;        
+      }
+      
+      int num = queue.size();
+      
+      if (num == 0) {
+        break;
+      }
+      
+      if (n < num) {
+        num = n;
+      }
+      
+      count += num;
+      n -= num;
+      
+      for (int i = 0; i < num; i++) {
+        buf[pos++] = queue.remove();
+      }
     }
+    
+    return count;
+  }
 }
