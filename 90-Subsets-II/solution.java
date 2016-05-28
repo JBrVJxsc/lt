ff8dba982610b5485d2a5ff61d63fb2e67afef7d
Xu ZHANG <laonawuli@126.com>
2016-05-27 22:35:45 -0700

    [90. Subsets II][Accepted]

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,33 +1,30 @@
 public class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        if (nums == null) {
-            throw new IllegalArgumentException("nums cannot be null.");
-        }
-        
-        List<List<Integer>> lists = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        Arrays.sort(nums);
-        
-        for (int i : nums) {
-            List<List<Integer>> temp = new ArrayList<>();
-            for (List<Integer> list : lists) {
-                List<Integer> l = new ArrayList<>(list);
-                l.add(i);
-                if (!set.contains(l)) {
-                    temp.add(l);
-                    set.add(l);
-                }
-            }
-            List<Integer> l = new ArrayList<>();
-            l.add(i);
-            if (!set.contains(l)) {
-                temp.add(l);
-                set.add(l);
-            }
-            lists.addAll(temp);
-        }
-        
-        lists.add(new ArrayList<>());
-        return lists;
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> lists = new ArrayList<>();
+    if (nums == null) {
+      return lists;
     }
+    Arrays.sort(nums);
+    Set<List<Integer>> set = new HashSet<>();
+    for (int i : nums) {
+      List<List<Integer>> temp = new ArrayList<>();
+      for (List<Integer> list : lists) {
+        List<Integer> l = new ArrayList<>(list);
+        l.add(i);
+        if (!set.contains(l)) {
+          set.add(l);
+          temp.add(l);
+        }
+      }
+      List<Integer> l = new ArrayList<>();
+      l.add(i);
+      if (!set.contains(l)) {
+        set.add(l);
+        temp.add(l);
+      }
+      lists.addAll(temp);
+    }
+    lists.add(new ArrayList<>());
+    return lists;
+  }
 }
