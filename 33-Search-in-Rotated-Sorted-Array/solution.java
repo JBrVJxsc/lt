e606ea42e5e4ad60a43cac780cfe763ffd33491a
Xu ZHANG <laonawuli@126.com>
2016-05-28 15:38:44 -0700

    [33. Search in Rotated Sorted Array][Accepted]

---
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+  public int search(int[] nums, int target) {
+    return binary(nums, target, 0, nums.length - 1);
+  }
+
+  private int binary(int[] nums, int target, int left, int right) {
+    if (left > right) {
+      return -1;
+    }
+    int mid = (left + right) / 2;
+    if (nums[mid] == target) {
+      return mid;
+    }
+    
+//     if (mid == 0) {
+//       return binary(nums, target, mid + 1, right);
+//     }
+    
+//     if (mid == nums.length - 1) {
+//       return binary(nums, target, left, mid - 1);
+//     }
+    
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        return binary(nums, target, left, mid - 1);
+      } else {
+        return binary(nums, target, mid + 1, right);
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        return binary(nums, target, mid + 1, right);
+      } else {
+        return binary(nums, target, left, mid - 1);
+      }
+    }
+  }
+}
