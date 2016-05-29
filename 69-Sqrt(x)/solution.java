5dcf414efc875954cb1f4c04ce17a1d60d9078bd
Xu ZHANG <laonawuli@126.com>
2016-05-28 23:45:44 -0700

    [69. Sqrt(x)][Accepted]

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -12,11 +12,11 @@ public class Solution {
     if (mid == x / mid) {
       return mid;
     } else if (mid > x / mid) {
-      return binary(left, mid, x);
+      return binary(left, mid - 1, x);
     } else if (mid + 1 > x / (mid + 1)) {
       return mid;
     } else {
-      return binary(mid, right, x);
+      return binary(mid + 1, right, x);
     }
   }
 }
