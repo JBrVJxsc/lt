bb8c7c41fed709e5180b12c86264db278dc7e30c
Xu ZHANG <laonawuli@126.com>
2016-05-29 10:52:26 -0700

    [85. Maximal Rectangle][Accepted]

--- 85-Maximal-Rectangle/solution.java
+++ 85-Maximal-Rectangle/solution.java
@@ -1,47 +1,51 @@
 public class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
-            return 0;
-        }
-        int[][] m = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < m[0].length; i++) {
-            if (matrix[0][i] == '1') {
-                m[0][i] = 1;
-            }
-        }
-        for (int i = 1; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                if (matrix[i][j] == '1') {
-                    m[i][j] = m[i - 1][j] + 1;
-                }
-            }
-        }
-        int max = 0;
-        
-        for (int i = 0; i < m.length; i++) {
-            max = Math.max(max, max(m[i]));
-        }
-        
-        return max;
+  public int maximalRectangle(char[][] matrix) {
+    if (matrix.length == 0 || matrix[0].length == 0) {
+      return 0;
+    }
+    int row = matrix.length;
+    int col = matrix[0].length;
+    int[][] heights = new int[row][col];
+    
+    for (int i = 0; i < col; i++) {
+      heights[0][i] = matrix[0][i] - '0';
     }
     
-    private int max(int[] heights) {
-        int[] h = new int[heights.length + 1];
-        for (int i = 0; i < heights.length; i++) {
-            h[i] = heights[i];
+    for (int i = 1; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (matrix[i][j] == '1') {
+          heights[i][j] = heights[i - 1][j] + 1;
         }
-        int max = 0;
-        Stack<Integer> s = new Stack<>();
-        int i = 0;
-        while (i < h.length) {
-            if (s.isEmpty() || h[s.peek()] <= h[i]) {
-                s.push(i++);
-            } else {
-                int top = s.pop();
-                int area = h[top] * (s.isEmpty() ? i : i - s.peek() - 1);
-                max = Math.max(max, area);
-            }
-        }
-        return max;
+      }
     }
+    
+    int max = 0;
+    for (int i = 0; i < row; i++) {
+      max = Math.max(max, max(heights[i]));
+    }
+    
+    return max;
+  }
+  
+  private int max(int[] h) {
+    int max = 0;
+    int[] heights = new int[h.length + 1];
+    for (int i = 0; i < h.length; i++) {
+      heights[i] = h[i];
+    }
+      
+    Stack<Integer> stack = new Stack<>();
+    int i = 0;
+    while (i < heights.length) {
+      if (stack.isEmpty() || heights[stack.peek()] <= heights[i]) {
+        stack.push(i++);
+      } else {
+        int height = heights[stack.pop()];
+        int len = stack.isEmpty() ? i : i - stack.peek() - 1;
+        max = Math.max(max, height * len);
+      }
+    }
+    
+    return max;
+  }
 }
