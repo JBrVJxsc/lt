28f7cff3a835e373088edacdcbb53926bf8e9e1a
Xu ZHANG <laonawuli@126.com>
2016-05-29 15:55:58 -0700

    [80. Remove Duplicates from Sorted Array II][Accepted]

---
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+  public int removeDuplicates(int[] nums) {
+    if (nums == null) {
+      return -1;
+    }
+    if (nums.length < 2) {
+      return nums.length;
+    }
+    
+    int cur = 2;
+    for (int i = cur; i < nums.length; i++) {
+      if (nums[i] != nums[cur - 2]) {
+        nums[cur++] = nums[i];
+      }
+    }
+    
+    return cur;
+  }
+}
