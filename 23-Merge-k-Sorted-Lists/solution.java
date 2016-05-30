337865b1bff5e6770d31dd0263514a6fb17c192d
Xu ZHANG <laonawuli@126.com>
2016-05-29 20:00:01 -0700

    [23. Merge k Sorted Lists][Accepted]

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -1,20 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
   public ListNode mergeKLists(ListNode[] lists) {
-      if (lists.length == 0) {
-          return null;
-      }
+    if (lists == null || lists.length == 0) {
+      return null;
+    }
+    
     Queue<ListNode> pq = new PriorityQueue<>((a, b) -> {
       return Integer.compare(a.val, b.val);
     });
+    
     for (ListNode node : lists) {
-        if (node != null) {
-      pq.add(node);
-        }
+      if (node != null) {
+        pq.add(node);
+      }
     }
     
     ListNode dummy = new ListNode(-1);
     ListNode next = dummy;
-    
     while (!pq.isEmpty()) {
       ListNode node = pq.remove();
       next.next = node;
@@ -22,7 +31,6 @@ public class Solution {
       if (node != null) {
         pq.add(node);
       }
-      next.next.next = null;
       next = next.next;
     }
     
