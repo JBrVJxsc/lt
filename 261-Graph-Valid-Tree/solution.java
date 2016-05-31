71ea4554bdcd988f8fba49b2f742ba04077c0099
Xu ZHANG <laonawuli@126.com>
2016-05-30 21:14:53 -0700

    [261. Graph Valid Tree][Accepted]

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,53 +1,43 @@
 public class Solution {
-    public boolean validTree(int n, int[][] edges) {
-        if (n < 2) {
-            return true;
-        }
-        
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for (int[] edge: edges) {
-            Set<Integer> set = map.get(edge[0]);
-            if (set == null) {
-                set = new HashSet<>();
-            }
-            set.add(edge[1]);
-            map.put(edge[0], set);
-            
-            set = map.get(edge[1]);
-            if (set == null) {
-                set = new HashSet<>();
-            }
-            set.add(edge[0]);
-            map.put(edge[1], set);
-        }
-        
-        int count = 1;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        visited[0] = true;
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-            Set<Integer> set = map.get(node);
-            if (set == null) {
-                return false;
-            }
-            
-            int num = 0;
-            for (int i : set) {
-                if (visited[i]) {
-                    num++;
-                } else {
-                    count++;
-                    queue.add(i);
-                    visited[i] = true;
-                }
-                if (num > 1) {
-                    return false;
-                }
-            }
-        }
-        
-        return count == n;
+  public boolean validTree(int n, int[][] edges) {
+    if (n < 2) {
+      return true;
     }
+    
+    if (edges == null || edges.length == 0 || edges[0].length == 0) {
+      return false;
+    }
+    
+    Map<Integer, Set<Integer>> map = new HashMap<>();
+    for (int[] edge : edges) {
+      if (map.get(edge[0]) == null) {
+        map.put(edge[0], new HashSet<>());
+      }
+      map.get(edge[0]).add(edge[1]);
+      if (map.get(edge[1]) == null) {
+        map.put(edge[1], new HashSet<>());
+      }
+      map.get(edge[1]).add(edge[0]);
+    }
+    
+    Set<Integer> set = new HashSet<>();
+    dfs(0, map, set);
+    
+    if (set.size() == n) {
+      if (edges.length == n - 1) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  private void dfs(int node, Map<Integer, Set<Integer>> map, Set<Integer> set) {
+    if (set.contains(node) || map.get(node) == null) {
+      return;
+    }
+    set.add(node);
+    for (int i : map.get(node)) {
+      dfs(i, map, set);
+    }
+  }
 }
