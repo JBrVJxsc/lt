993f156345bd161e180e24917e8bcdd133a06d12
Xu ZHANG <laonawuli@126.com>
2016-06-01 23:07:54 -0700

    [158. Read N Characters Given Read4 II - Call multiple times][Accepted]

--- 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
+++ 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
@@ -11,7 +11,7 @@ public class Solution extends Reader4 {
   
   public int read(char[] buf, int n) {
     char[] temp = new char[4];
-    int count = 0;
+    int counter = 0;
     int pos = 0;
     
     while (n > 0) {
@@ -23,23 +23,22 @@ public class Solution extends Reader4 {
       }
       
       int num = queue.size();
-      
-      if (num == 0) {
-        break;
-      }
-      
       if (n < num) {
         num = n;
       }
       
-      count += num;
+      counter += num;
       n -= num;
       
       for (int i = 0; i < num; i++) {
         buf[pos++] = queue.remove();
       }
+      
+      if (num < 4) {
+        break;
+      }
     }
     
-    return count;
+    return counter;
   }
 }
