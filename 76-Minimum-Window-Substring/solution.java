119aab8bf2678284ae00a0d546f90b114c5e8596
Xu ZHANG <laonawuli@126.com>
2016-06-03 10:06:00 -0700

    [76. Minimum Window Substring][Accepted]

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,46 +1,54 @@
 public class Solution {
   public String minWindow(String s, String t) {
+    if (s == null || t == null) {
+      return "";
+    }
+    
     Map<Character, Integer> dict = new HashMap<>();
+    Map<Character, Integer> found = new HashMap<>();
+    
     for (char c : t.toCharArray()) {
       int num = dict.getOrDefault(c, 0) + 1;
       dict.put(c, num);
     }
     
-    Map<Character, Integer> found = new HashMap<>();
     int l = 0;
     int r = 0;
-    int count = 0;
+    int counter = 0;
     int curLen = Integer.MAX_VALUE;
     String result = "";
     
     while (r < s.length()) {
-      while (r < s.length() && count != t.length()) {
+      while (r < s.length() && counter != t.length()) {
         char c = s.charAt(r++);
-        if (dict.get(c) != null) {
-          Integer foundNum = found.getOrDefault(c, 0);
-          if (foundNum < dict.get(c)) {
-            count++;
-          }
-          found.put(c, foundNum + 1);
+        if (!dict.containsKey(c)) {
+          continue;
         }
+        int foundNum = found.getOrDefault(c, 0);
+        if (foundNum < dict.get(c)) {
+          counter++;
+        }
+        found.put(c, foundNum + 1);
       }
       
-      while (count == t.length()) {
+      while (l < s.length() && counter == t.length()) {
         char c = s.charAt(l++);
-        if (dict.get(c) != null) {
-          int foundNum = found.get(c);
-          if (foundNum == dict.get(c)) {
-            count--;
-            String str = s.substring(l - 1, r);
-            if (str.length() < curLen) {
-              curLen = str.length();
-              result = str;
-            }
+        if (!dict.containsKey(c)) {
+          continue;
+        }
+        int foundNum = found.getOrDefault(c, 0);
+        if (foundNum == dict.get(c)) {
+          counter--;
+          String sub = s.substring(l - 1, r);
+          if (sub.length() < curLen) {
+            curLen = sub.length();
+            result = sub;
           }
-          found.put(c, foundNum - 1);
-        }        
+        }
+        found.put(c, foundNum - 1);
       }
     }
+    
     return result;
   }
 }
