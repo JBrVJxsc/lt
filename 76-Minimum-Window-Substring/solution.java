473d152d451dac3ae12f4cf416fffa69ab88a1d1
Xu ZHANG <laonawuli@126.com>
2016-06-03 00:28:42 -0700

    [76. Minimum Window Substring][Accepted]

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,47 +1,46 @@
 public class Solution {
   public String minWindow(String s, String t) {
-    Map<Character, Integer> map = new HashMap<>();
+    Map<Character, Integer> dict = new HashMap<>();
     for (char c : t.toCharArray()) {
-      int num = map.getOrDefault(c, 0) + 1;
-      map.put(c, num);
+      int num = dict.getOrDefault(c, 0) + 1;
+      dict.put(c, num);
     }
     
     Map<Character, Integer> found = new HashMap<>();
-    String result = "";
     int l = 0;
     int r = 0;
-    int counter = 0;
+    int count = 0;
     int curLen = Integer.MAX_VALUE;
+    String result = "";
     
     while (r < s.length()) {
-      while (r < s.length() && counter != t.length()) {
+      while (r < s.length() && count != t.length()) {
         char c = s.charAt(r++);
-        if (map.get(c) != null) {
-          int num = found.getOrDefault(c, 0);
-          if (num < map.get(c)) {
-            counter++;
+        if (dict.get(c) != null) {
+          int foundNum = found.getOrDefault(c, 0);
+          if (foundNum < dict.get(c)) {
+            count++;
           }
-          found.put(c, num + 1);
+          found.put(c, foundNum + 1);
         }
       }
       
-      while (counter == t.length()) {
+      while (count == t.length()) {
         char c = s.charAt(l++);
-        if (map.get(c) != null) {
-          int num = found.get(c);
-          if (num == map.get(c)) {
-            counter--;
+        if (dict.get(c) != null) {
+          int foundNum = found.get(c);
+          if (foundNum == dict.get(c)) {
+            count--;
             String str = s.substring(l - 1, r);
             if (str.length() < curLen) {
               curLen = str.length();
               result = str;
             }
           }
-          found.put(c, num - 1);
-        }
+          found.put(c, foundNum - 1);
+        }        
       }
     }
-    
     return result;
   }
 }
