2f19a7c7dd5f98c459352e9940d85000b8e62bdd
Xu ZHANG <laonawuli@126.com>
2016-06-04 09:03:30 -0700

    [283. Move Zeroes][Accepted]

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -3,12 +3,14 @@ public class Solution {
     int cur = 0;
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] != 0) {
-        nums[cur++] = nums[i];
+        swap(nums, i, cur++);
       }
     }
-    
-    for (int i = cur; i < nums.length; i++) {
-      nums[i] = 0;
-    }
+  }
+  
+  private void swap(int[] nums, int a, int b) {
+    int temp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = temp;
   }
 }
