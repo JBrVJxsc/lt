f0a7388b545231e9ccadbcf07e09ab3f52ab3128
Xu ZHANG <laonawuli@126.com>
2016-06-04 08:51:42 -0700

    [47. Permutations II][Accepted]

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -1,29 +1,31 @@
 public class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> lists = new HashSet<>();
-        if (nums == null || nums.length == 0){
-            return new ArrayList<>();
-        }
-        
-        dfs(nums, lists, new ArrayList<>(), new boolean[nums.length]);
-        
-        return new ArrayList<>(lists);
+  public List<List<Integer>> permuteUnique(int[] nums) {
+    Set<List<Integer>> lists = new HashSet<>();
+    if (nums == null) {
+      return new ArrayList<>();
     }
     
-    private void dfs(int[] nums, Set<List<Integer>> lists, List<Integer> cur, boolean[] visited) {
-        if (cur.size() == nums.length) {
-            lists.add(new ArrayList<>(cur));
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            cur.add(nums[i]);
-            visited[i] = true;
-            dfs(nums, lists, cur, visited);
-            visited[i] = false;
-            cur.remove(cur.size() - 1);
-        }
+    Arrays.sort(nums);
+    dfs(nums, lists, new ArrayList<>(), new boolean[nums.length]);
+    
+    return new ArrayList<>(lists);
+  }
+  
+  private void dfs(int[] nums, Set<List<Integer>> lists, List<Integer> cur, boolean[] visited) {
+    if (cur.size() == nums.length) {
+      lists.add(new ArrayList<>(cur));
+      return;
     }
+    
+    for (int i = 0; i < nums.length; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      visited[i] = true;
+      cur.add(nums[i]);
+      dfs(nums, lists, cur, visited);
+      cur.remove(cur.size() - 1);
+      visited[i] =false;
+    }
+  }
 }
