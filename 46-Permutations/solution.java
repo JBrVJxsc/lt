8debd2d7fb0495f2fc75740537c65b133134a8f5
Xu ZHANG <laonawuli@126.com>
2016-06-04 08:33:02 -0700

    [46. Permutations][Accepted]

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,29 +1,30 @@
 public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> lists = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return lists;
-        }
-        
-        dfs(nums, new ArrayList<>(), lists, new boolean[nums.length]);
-        
-        return lists;
+  public List<List<Integer>> permute(int[] nums) {
+    if (nums == null) {
+      return new ArrayList<>();
+    }
+    List<List<Integer>> result = new ArrayList<>();
+    
+    dfs(nums, result, new ArrayList<>(), new boolean[nums.length]);
+    
+    return result;
+  }
+  
+  private void dfs(int[] nums, List<List<Integer>> result, List<Integer> cur, boolean[] visited) {
+    if (cur.size() == nums.length) {
+      result.add(new ArrayList<>(cur));
+      return;
     }
     
-    private void dfs(int[] nums, List<Integer> cur, List<List<Integer>> lists, boolean[] visited) {
-        if (cur.size() == nums.length) {
-            lists.add(new ArrayList<>(cur));
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            cur.add(nums[i]);
-            visited[i] = true;
-            dfs(nums, cur, lists, visited);
-            visited[i] = false;
-            cur.remove(cur.size() - 1);
-        }
+    for (int i = 0; i < nums.length; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      visited[i] = true;
+      cur.add(nums[i]);
+      dfs(nums, result, cur, visited);
+      cur.remove(cur.size() - 1);
+      visited[i] = false;
     }
+  }
 }
