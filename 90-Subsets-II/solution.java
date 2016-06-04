4839c19f0a0aa647ba10a1a8be2a6906ae6ee509
Xu ZHANG <laonawuli@126.com>
2016-06-04 00:09:13 -0700

    [90. Subsets II][Accepted]

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,30 +1,31 @@
 public class Solution {
   public List<List<Integer>> subsetsWithDup(int[] nums) {
-    List<List<Integer>> lists = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
     if (nums == null) {
-      return lists;
+      return result;
     }
     Arrays.sort(nums);
-    Set<List<Integer>> set = new HashSet<>();
-    for (int i : nums) {
-      List<List<Integer>> temp = new ArrayList<>();
-      for (List<Integer> list : lists) {
-        List<Integer> l = new ArrayList<>(list);
-        l.add(i);
-        if (!set.contains(l)) {
-          set.add(l);
-          temp.add(l);
-        }
-      }
-      List<Integer> l = new ArrayList<>();
-      l.add(i);
-      if (!set.contains(l)) {
-        set.add(l);
-        temp.add(l);
-      }
-      lists.addAll(temp);
+    result.add(new ArrayList<>());
+    for (int i = 1; i <= nums.length; i++) {
+      dfs(nums, 0, i, result, new ArrayList<>());
     }
-    lists.add(new ArrayList<>());
-    return lists;
+    return result;
+  }
+  
+  private void dfs(int[] nums, int start, int len, List<List<Integer>> result, List<Integer> cur) {
+    if (cur.size() == len) {
+      result.add(new ArrayList<>(cur));
+      return;
+    }
+    int i = start;
+    while (i < nums.length) {
+      cur.add(nums[i]);
+      dfs(nums, i + 1, len, result, cur);
+      cur.remove(cur.size() - 1);
+      i++;
+      while (i < nums.length && nums[i] == nums[i - 1]) {
+        i++;
+      }
+    }
   }
 }
