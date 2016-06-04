fb3a06a6441e49cccc4c2c12e4e0bad5fbcc901d
Xu ZHANG <laonawuli@126.com>
2016-06-04 09:56:22 -0700

    [91. Decode Ways][Accepted]

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -6,24 +6,22 @@ public class Solution {
     
     int[] dp = new int[s.length()];
     
-    for (int i = s.length() - 1; 0 <= i; i--) {
+    for (int i = s.length() - 1; i >= 0; i--) {
       char c = s.charAt(i);
       if (c == '0') {
         continue;
       }
-      
+
       if (i == s.length() - 1) {
-        dp[i] += 1;
-      } else {
-        dp[i] += dp[i + 1];
-        if (c == '1' || c == '2' && s.charAt(i + 1) < '7') {
-          if (i == s.length() - 2) {
-            dp[i] += 1;
-          } else {
-            dp[i] += dp[i + 2];
-          }
-        }
+        dp[i] = 1;
+        continue;
       }
+      
+      dp[i] += dp[i + 1];
+
+      if (c == '1' || c == '2' && s.charAt(i + 1) < '7') {
+        dp[i] += i == s.length() - 2 ? 1 : dp[i + 2];
+      }      
     }
     
     return dp[0];
