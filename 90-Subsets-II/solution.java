081bea955f3551ad6418fc08addf09e63f346cb2
Xu ZHANG <laonawuli@126.com>
2016-06-04 08:17:54 -0700

    [90. Subsets II][Accepted]

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -6,21 +6,24 @@ public class Solution {
     }
     Arrays.sort(nums);
     result.add(new ArrayList<>());
-    for (int i = 1; i <= nums.length; i++) {
-      dfs(nums, 0, i, result, new ArrayList<>());
+    
+    for (int len = 1; len <= nums.length; len++) {
+      dfs(nums, len, 0, result, new ArrayList<>());
     }
+    
     return result;
   }
   
-  private void dfs(int[] nums, int start, int len, List<List<Integer>> result, List<Integer> cur) {
+  private void dfs(int[] nums, int len, int start, List<List<Integer>> result, List<Integer> cur) {
     if (cur.size() == len) {
       result.add(new ArrayList<>(cur));
       return;
     }
+    
     int i = start;
     while (i < nums.length) {
       cur.add(nums[i]);
-      dfs(nums, i + 1, len, result, cur);
+      dfs(nums, len, i + 1, result, cur);
       cur.remove(cur.size() - 1);
       i++;
       while (i < nums.length && nums[i] == nums[i - 1]) {
