8db703e7dfe794af140d13fea341e1ba50f73f97
Xu ZHANG <laonawuli@126.com>
2016-06-04 19:50:48 -0700

    [43. Multiply Strings][Accepted]

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -1,32 +1,37 @@
 public class Solution {
+  
+//   public static void main(String[] args) {
+//     System.out.println(new Solution().multiply("1234", "5678"));
+//   }
+  
   public String multiply(String num1, String num2) {
-    int m = num1.length();
-    int n = num2.length();
-    int[] result = new int[m + n];
+    int l1 = num1.length();
+    int l2 = num2.length();
+    int[] result = new int[l1 + l2];
     
-    for (int i = m - 1; i >= 0; i--) {
-      for (int j = n - 1; j >= 0; j--) {
+    for (int i = l1 - 1; i >= 0; i--) {
+      for (int j = l2 - 1; j >= 0; j--) {
         int p1 = i + j;
         int p2 = i + j + 1;
-        int sum = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-        sum += result[p2];
-        result[p2] = sum % 10;
-        result[p1] += sum / 10;
+        int num = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+        num += result[p2];
+        
+        result[p1] += num / 10;
+        result[p2] = num % 10;
       }
     }
     
     StringBuilder sb = new StringBuilder();
-    for (int i : result) {
-      if (sb.length() == 0 && i == 0) {
-        continue;
-      }
-      sb.append(i);
+    int i = 0;
+    while (i < result.length && result[i] == 0) {
+      i++;
     }
-    
+    for (; i < result.length; i++) {
+      sb.append(result[i]);
+    }
     if (sb.length() == 0) {
       return "0";
     }
-    
     return sb.toString();
   }
 }
