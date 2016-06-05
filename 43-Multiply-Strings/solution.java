47f7b709bca073abc46e67ef28807e3580e96bd9
Xu ZHANG <laonawuli@126.com>
2016-06-05 11:14:55 -0700

    [43. Multiply Strings][Accepted]

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -1,37 +1,38 @@
 public class Solution {
-  
-//   public static void main(String[] args) {
-//     System.out.println(new Solution().multiply("1234", "5678"));
-//   }
-  
   public String multiply(String num1, String num2) {
-    int l1 = num1.length();
-    int l2 = num2.length();
-    int[] result = new int[l1 + l2];
+    if (num1 == null || num2 == null) {
+      throw new IllegalArgumentException("num1 and num2 cannot be null.");
+    }
     
-    for (int i = l1 - 1; i >= 0; i--) {
-      for (int j = l2 - 1; j >= 0; j--) {
+    int len1 = num1.length();
+    int len2 = num2.length();
+    int[] result = new int[len1 + len2];
+  
+    for (int i = len1 - 1; i >= 0; i--) {
+      for (int j = len2 - 1; j >= 0; j--) {
         int p1 = i + j;
         int p2 = i + j + 1;
-        int num = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-        num += result[p2];
-        
-        result[p1] += num / 10;
-        result[p2] = num % 10;
+        int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+        mul += result[p2];
+        result[p1] += mul / 10;
+        result[p2] = mul % 10;
       }
     }
     
-    StringBuilder sb = new StringBuilder();
     int i = 0;
     while (i < result.length && result[i] == 0) {
       i++;
     }
+    
+    StringBuilder sb = new StringBuilder();
     for (; i < result.length; i++) {
       sb.append(result[i]);
     }
+    
     if (sb.length() == 0) {
       return "0";
     }
+    
     return sb.toString();
   }
 }
