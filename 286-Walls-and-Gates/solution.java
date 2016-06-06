001670be4ef6e6a6e157a58127ddfebb0fce6263
Xu ZHANG <laonawuli@126.com>
2016-06-05 19:48:34 -0700

    [286. Walls and Gates][Accepted]

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,47 +1,46 @@
 public class Solution {
+  
+  private int INF = Integer.MAX_VALUE;
+  
   public void wallsAndGates(int[][] rooms) {
-    if (rooms == null || rooms.length == 0 || rooms[0].length == 0) {
+    if (rooms == null) {
+      return;
+    }
+    if (rooms.length == 0 || rooms[0].length == 0) {
       return;
     }
     
-    int row = rooms.length;
-    int col = rooms[0].length;
-    boolean[][] visited = new boolean[row][col];
+    Queue<int[]> q = new LinkedList<>();
     
-    Queue<int[]> queue = new LinkedList<>();
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
+    for (int i = 0; i < rooms.length; i++) {
+      for (int j = 0; j < rooms[i].length; j++) {
         if (rooms[i][j] == 0) {
-          queue.add(new int[]{i, j});
+          q.add(new int[]{i, j});
         }
       }
     }
     
-    while (!queue.isEmpty()) {
-      int[] pos = queue.remove();
-      int x = pos[0];
-      int y = pos[1];
-      int dis = rooms[x][y];
+    while (!q.isEmpty()) {
+      int[] p = q.remove();
+      int x = p[0];
+      int y = p[1];
+      int d = rooms[x][y];
       
-      if (x > 0 && !visited[x - 1][y] && rooms[x - 1][y] > 0) {
-        queue.add(new int[]{x - 1, y});
-        rooms[x - 1][y] = dis + 1;
-        visited[x - 1][y] = true;
+      if (x > 0 && rooms[x - 1][y] == INF) {
+        q.add(new int[]{x - 1, y});
+        rooms[x - 1][y] = d + 1;
       }
-      if (y > 0 && !visited[x][y - 1] && rooms[x][y - 1] > 0) {
-        queue.add(new int[]{x, y - 1});
-        rooms[x][y - 1] = dis + 1;
-        visited[x][y - 1] = true;
+      if (x < rooms.length - 1 && rooms[x + 1][y] == INF) {
+        q.add(new int[]{x + 1, y});
+        rooms[x + 1][y] = d + 1;
       }
-      if (x < rooms.length - 1 && !visited[x + 1][y] && rooms[x + 1][y] > 0) {
-        queue.add(new int[]{x + 1, y});
-        rooms[x + 1][y] = dis + 1;
-        visited[x + 1][y] = true;
+      if (y > 0 && rooms[x][y - 1] == INF) {
+        q.add(new int[]{x, y - 1});
+        rooms[x][y - 1] = d + 1;
       }
-      if (y < rooms[0].length - 1 && !visited[x][y + 1] && rooms[x][y + 1] > 0) {
-        queue.add(new int[]{x, y + 1});
-        rooms[x][y + 1] = dis + 1;
-        visited[x][y + 1] = true;
+      if (y < rooms[x].length - 1 && rooms[x][y + 1] == INF) {
+        q.add(new int[]{x, y + 1});
+        rooms[x][y + 1] = d + 1;
       }      
     }
   }
