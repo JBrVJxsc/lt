5a5dcfaefdac21996cf57369f3515cce0bc84535
Xu ZHANG <laonawuli@126.com>
2016-06-06 09:30:04 -0700

    [257. Binary Tree Paths][Accepted]

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -9,23 +9,34 @@
  */
 public class Solution {
   public List<String> binaryTreePaths(TreeNode root) {
-    if (root == null) {
-      return new ArrayList<>();
-    }
-    if (root.left == null && root.right == null) {
-      List<String> list = new ArrayList<>();
-      list.add(String.valueOf(root.val));
-      return list;
-    }
     List<String> result = new ArrayList<>();
-    List<String> list = binaryTreePaths(root.left);
-    for (String str : list) {
-      result.add(String.valueOf(root.val) + "->" + str);
+    if (root == null) {
+      return result;
     }
-    list = binaryTreePaths(root.right);
-    for (String str : list) {
-      result.add(String.valueOf(root.val) + "->" + str);
-    }
+    StringBuilder path = new StringBuilder();
+    path.append(root.val);
+    dfs(root, path, result);
     return result;
+  }
+  
+  private void dfs(TreeNode root, StringBuilder path, List<String> result) {
+    if (root.left == null && root.right == null) {
+      result.add(path.toString());
+      return;
+    }
+    if (root.left != null) {
+      int len = path.length();
+      path.append("->");
+      path.append(root.left.val);
+      dfs(root.left, path, result);
+      path.setLength(len);
+    }
+    if (root.right != null) {
+      int len = path.length();
+      path.append("->");
+      path.append(root.right.val);
+      dfs(root.right, path, result);
+      path.setLength(len);
+    }    
   }
 }
