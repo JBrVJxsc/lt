3e10fa968bcfc21d1d6ae976fbec2bc43aa51d59
Xu ZHANG <laonawuli@126.com>
2016-06-06 09:09:27 -0700

    [191. Number of 1 Bits][Accepted]

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
-  // you need to treat n as an unsigned value
-  public int hammingWeight(int n) {
-    int count = 0;
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+    int counter = 0;
     
     while (n != 0) {
       if ((n & 1) == 1) {
-        count++;
+        counter++;
       }
       n = n >>> 1;
     }
     
-    return count;
-  }
+    return counter;        
+    }
 }
