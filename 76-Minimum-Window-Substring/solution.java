9e1e1d9a3909be1c502b064842677d5af2ee1fda
Xu ZHANG <laonawuli@126.com>
2016-06-07 09:50:23 -0700

    [76. Minimum Window Substring][Accepted]

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,51 +1,52 @@
 public class Solution {
   public String minWindow(String s, String t) {
     if (s == null || t == null) {
+      throw new IllegalArgumentException("s and t cannot be null.");
+    }
+    
+    if (t.length() > s.length()) {
       return "";
     }
     
-    Map<Character, Integer> dict = new HashMap<>();
+    Map<Character, Integer> needs = new HashMap<>();
     Map<Character, Integer> found = new HashMap<>();
     
     for (char c : t.toCharArray()) {
-      int num = dict.getOrDefault(c, 0) + 1;
-      dict.put(c, num);
+      int num = needs.getOrDefault(c, 0) + 1;
+      needs.put(c, num);
     }
     
+    String result = "";
+    int counter = 0;
     int l = 0;
     int r = 0;
-    int counter = 0;
-    int curLen = Integer.MAX_VALUE;
-    String result = "";
-    
     while (r < s.length()) {
       while (r < s.length() && counter != t.length()) {
         char c = s.charAt(r++);
-        if (!dict.containsKey(c)) {
+        if (!needs.containsKey(c)) {
           continue;
         }
-        int foundNum = found.getOrDefault(c, 0);
-        if (foundNum < dict.get(c)) {
+        int num = found.getOrDefault(c, 0);
+        if (num < needs.get(c)) {
           counter++;
         }
-        found.put(c, foundNum + 1);
+        found.put(c, num + 1);
       }
       
       while (l < s.length() && counter == t.length()) {
         char c = s.charAt(l++);
-        if (!dict.containsKey(c)) {
+        if (!needs.containsKey(c)) {
           continue;
         }
-        int foundNum = found.getOrDefault(c, 0);
-        if (foundNum == dict.get(c)) {
+        int num = found.get(c);
+        if (num == needs.get(c)) {
           counter--;
           String sub = s.substring(l - 1, r);
-          if (sub.length() < curLen) {
-            curLen = sub.length();
+          if (result.equals("") || sub.length() < result.length()) {
             result = sub;
           }
         }
-        found.put(c, foundNum - 1);
+        found.put(c, num - 1);        
       }
     }
     
