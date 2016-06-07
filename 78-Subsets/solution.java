acbb2c05c2f483ba32ef9aaffe1c1492ca52ecf8
Xu ZHANG <laonawuli@126.com>
2016-06-07 09:16:30 -0700

    [78. Subsets][Accepted]

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,30 +1,30 @@
 public class Solution {
-public List<List<Integer>> subsets(int[] nums) {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
     if (nums == null) {
-      return new ArrayList<>();
+      return result;
     }
     
     Arrays.sort(nums);
-    List<List<Integer>> result = new ArrayList<>();
     result.add(new ArrayList<>());
     
     for (int len = 1; len <= nums.length; len++) {
-      dfs(nums, 0, len, result, new ArrayList<>());
+      dfs(nums, len, 0, new ArrayList<>(), result);
     }
     
     return result;
   }
   
-  private void dfs(int[] nums, int start, int len, List<List<Integer>> lists, List<Integer> cur) {
-    if (cur.size() == len) {
-      lists.add(new ArrayList<>(cur));
+  private void dfs(int[] nums, int len, int index, List<Integer> path, List<List<Integer>> result) {
+    if (path.size() == len) {
+      result.add(new ArrayList<>(path));
       return;
     }
     
-    for (int i = start; i < nums.length; i++) {
-      cur.add(nums[i]);
-      dfs(nums, i + 1, len, lists, cur);
-      cur.remove(cur.size() - 1);
+    for (int i = index; i < nums.length; i++) {
+      path.add(nums[i]);
+      dfs(nums, len, i + 1, path, result);
+      path.remove(path.size() - 1);
     }
   }
 }
