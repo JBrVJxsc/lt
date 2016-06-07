0c9809d7ca117c7560425d02d39d6e129478029c
Xu ZHANG <laonawuli@126.com>
2016-06-06 22:04:50 -0700

    [157. Read N Characters Given Read4][Accepted]

--- 157-Read-N-Characters-Given-Read4/solution.java
+++ 157-Read-N-Characters-Given-Read4/solution.java
@@ -2,28 +2,28 @@
       int read4(char[] buf); */
 
 public class Solution extends Reader4 {
-  /**
+    /**
      * @param buf Destination buffer
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
   public int read(char[] buf, int n) {
     char[] temp = new char[4];
-    int pos = 0;
     int counter = 0;
+    int pos = 0;
     
     while (n > 0) {
       int num = read4(temp);
-      if (n < num) {
+      if (num > n) {
         num = n;
       }
-      
-      counter += num;
-      n -= num;
       
       for (int i = 0; i < num; i++) {
         buf[pos++] = temp[i];
       }
+      
+      n -= num;
+      counter += num;
       
       if (num < 4) {
         break;
