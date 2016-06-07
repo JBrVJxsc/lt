3a5dce920802170a09646572fd03672239b7ffed
Xu ZHANG <laonawuli@126.com>
2016-06-07 09:38:46 -0700

    [111. Minimum Depth of Binary Tree][Accepted]

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -8,16 +8,25 @@
  * }
  */
 public class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        if (root.left == null) {
-            return minDepth(root.right) + 1;
-        } else if (root.right == null) {
-            return minDepth(root.left) + 1;
-        } else {
-            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-        }
+  
+  int min = Integer.MAX_VALUE;
+  
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
+    dfs(root, 1);
+    return min;
+  }
+  
+  private void dfs(TreeNode root, int level) {
+    if (root == null) {
+      return;
+    }
+    if (root.left == null && root.right == null) {
+      min = Math.min(min, level);
+    }
+    dfs(root.left, level + 1);
+    dfs(root.right, level + 1);
+  }
 }
