34858bb2a7ca33a5f1b9b3602429918a04563354
Xu ZHANG <laonawuli@126.com>
2016-06-08 21:06:28 -0700

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,42 +1,56 @@
 public class Solution {
-  public boolean isMatch(String s, String p) {
-    if (s == null || p == null) {
+  public boolean isMatch(String str, String pattern) {
+    if (str == null || pattern == null) {
+      throw new IllegalArgumentException("str or pattern cannot be null.");
+    }
+    
+    if (pattern.length() == 0) {
+      return str.length() == 0;
+    }
+    
+    if (pattern.length() == 1 || pattern.charAt(1) != '*') {
+      if (str.length() == 0) {
+        return false;
+      }
+      
+      if (isSame(str.charAt(0), pattern.charAt(0))) {
+        return isMatch(str.substring(1), pattern.substring(1));
+      }
+      
       return false;
     }
     
-    if (p.length() == 0) {
-      return s.length() == 0;
-    }
-    
-    if (p.length() == 1 || p.charAt(1) != '*') {
-      if (s.length() == 0) {
-        return false;
-      }
-      if (!equals(s.charAt(0), p.charAt(0))) {
-        return false;
-      }
-      return isMatch(s.substring(1), p.substring(1));
-    }
-    
-    if (p.charAt(1) == '*') {
-      if (isMatch(s, p.substring(2))) {
+    if (pattern.charAt(1) == '*') {
+      String sub = pattern.substring(2);
+      
+      if (isMatch(str, sub)) {
         return true;
       }
+      
       int i = 0;
-      while (i < s.length() && equals(s.charAt(i), p.charAt(0))) {
-        if (isMatch(s.substring(i + 1), p.substring(2))) {
+      while (i < str.length() && isSame(str.charAt(i), pattern.charAt(0))) {
+        String subStr = str.substring(i + 1);
+        if (isMatch(subStr, sub)) {
           return true;
         }
         i++;
       }
+      
+      return false;
     }
+    
     return false;
   }
   
-  private boolean equals(char a, char b) {
-    if (b == '.') {
+  private boolean isSame(char c1, char c2) {
+    if (c1 == c2) {
       return true;
     }
-    return a == b;
+    
+    if (c1 == '.' || c2 == '.') {
+      return true;
+    }
+    
+    return false;
   }
 }
