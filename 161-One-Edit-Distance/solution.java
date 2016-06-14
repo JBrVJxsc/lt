17ed2c05f179363a8b4a2788ba5cf81f02e49fc0
Xu ZHANG <laonawuli@126.com>
2016-06-13 19:22:00 -0700

    [161. One Edit Distance][Accepted]

--- 161-One-Edit-Distance/solution.java
+++ 161-One-Edit-Distance/solution.java
@@ -1,58 +1,48 @@
 public class Solution {
-  public boolean isOneEditDistance(String s, String t) {
-    if (s == null || t == null) {
-      throw new IllegalArgumentException("s and t cannot be null.");
-    }
-    
-    int len1 = s.length();
-    int len2 = t.length();
+  public boolean isOneEditDistance(String s1, String s2) {
+    int len1 = s1.length();
+    int len2 = s2.length();
     
     if (Math.abs(len1 - len2) > 1) {
       return false;
     }
     
+    int dif = Math.abs(len1 - len2);
     int i = 0;
     int j = 0;
-    int dif = 0;
+    int chance = 1;
     
-    if (len1 == len2) {
-      while (i < len1) {
-        if (s.charAt(i++) != t.charAt(j++)) {
-          dif++;
-        }
-      }
-    } else {
-      dif = 1;
-      int chance = 1;
-      while (i < len1 && j < len2) {
-        if (s.charAt(i) != t.charAt(j)) {
-          if (chance > 0) {
-            chance--;
-            if (len1 < len2) {
-              j++;
-            } else {
-              i++;
-            }
-            continue;
-          } else {
-            dif++;
-          }
-        }
+    while (i < len1 && j < len2) {
+      if (s1.charAt(i) == s2.charAt(j)) {
         i++;
         j++;
+      } else {
+        if (len1 < len2) {
+          if (chance > 0) {
+            chance--;
+            j++;
+          } else {
+            i++;
+            j++;
+            dif++;
+          }
+        } else if (len1 > len2) {
+          if (chance > 0) {
+            chance--;
+            i++;
+          } else {
+            i++;
+            j++;
+            dif++;
+          }          
+        } else {
+          i++;
+          j++;
+          dif++;
+        }
       }
     }
     
     return dif == 1;
   }
 }
-
-
-
-
-
-
-
-
-
-
