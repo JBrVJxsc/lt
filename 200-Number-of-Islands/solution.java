4bbe851264541ca7966f2af278746a95469d9208
Xu ZHANG <laonawuli@126.com>
2016-06-13 19:51:59 -0700

    [200. Number of Islands][Accepted]

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -4,35 +4,34 @@ public class Solution {
       return 0;
     }
     
-    int counter = 0;
-    boolean[][] visited = new boolean[grid.length][grid[0].length];
+    int row = grid.length;
+    int col = grid[0].length;
+    int num = 0;
+    boolean[][] visited = new boolean[row][col];
     
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        if (grid[i][j] == '1') {
-          if (dfs(grid, i, j, visited) > 0) {
-            counter++;
-          }
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (dfs(grid, i, j, row, col, visited) > 0) {
+          num++;
         }
       }
     }
     
-    return counter;
+    return num;
   }
   
-  private int dfs(char[][] grid, int x, int y, boolean[][] visited) {
-    if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || visited[x][y]) {
+  private int dfs(char[][] grid, int x, int y, int row, int col, boolean[][] visited) {
+    if (x < 0 || x >= row || y < 0 || y >= col || visited[x][y] || grid[x][y] != '1') {
       return 0;
     }
-    if (grid[x][y] == '0') {
-      return 0;
-    }
+    
     visited[x][y] = true;
-    int num = 1;
-    num += dfs(grid, x + 1, y, visited);
-    num += dfs(grid, x - 1, y, visited);
-    num += dfs(grid, x, y + 1, visited);
-    num += dfs(grid, x, y - 1, visited);
-    return num;
+    int size = 1;
+    size += dfs(grid, x - 1, y, row, col, visited);
+    size += dfs(grid, x + 1, y, row, col, visited);
+    size += dfs(grid, x, y - 1, row, col, visited);
+    size += dfs(grid, x, y + 1, row, col, visited);
+    
+    return size;
   }
 }
