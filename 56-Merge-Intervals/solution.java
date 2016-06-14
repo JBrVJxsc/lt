d473386127a11597268881d540c12b2854a38582
Xu ZHANG <laonawuli@126.com>
2016-06-13 18:51:50 -0700

    [56. Merge Intervals][Accepted]

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -1,26 +1,38 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
 public class Solution {
   public List<Interval> merge(List<Interval> intervals) {
-    List<Interval> list = new ArrayList<>();
+    List<Interval> result = new ArrayList<>();
+    if (intervals == null || intervals.size() < 1) {
+      return result;
+    }
     
     Collections.sort(intervals, (a, b) -> {
       return Integer.compare(a.start, b.start);
     });
     
-    for (Interval i : intervals) {
-      if (list.size() == 0) {
-        list.add(i);
-      } else {
-        Interval last = list.get(list.size() - 1);
-        if (i.end <= last.end) {
-          continue;
-        } else if (i.start <= last.end) {
-          last.end = i.end;
-        } else {
-          list.add(i);
-        }
+    result.add(intervals.get(0));
+    
+    for (int i = 1; i < intervals.size(); i++) {
+      Interval pre = result.get(result.size() - 1);
+      Interval cur = intervals.get(i);
+      if (cur.end <= pre.end) {
+        continue;
+      }
+      if (cur.start <= pre.end) {
+        pre.end = cur.end;
+      } else if (pre.end < cur.start) {
+        result.add(cur);
       }
     }
     
-    return list;
+    return result;
   }
 }
