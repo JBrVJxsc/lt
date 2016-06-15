8bbb63b83af6bace26a8a653aa72321644452319
Xu ZHANG <laonawuli@126.com>
2016-06-14 23:31:32 -0700

    [273. Integer to English Words][Accepted]

--- 273-Integer-to-English-Words/solution.java
+++ 273-Integer-to-English-Words/solution.java
@@ -1,30 +1,34 @@
 public class Solution {
-  private String[] digits = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-  private String[] lessThan20 = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-  private String[] lessThan100 = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-  
+  private String[] lessThanTen = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+  private String[] lessThenTwenty = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private String[] lessThenHundred = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
   public String numberToWords(int num) {
     if (num == 0) {
       return "Zero";
     }
-    return convert(num);
+    return helper(num);
   }
-
-  private String convert(int n) {
-    String str = "";
-    if (n < 10) {
-      str = digits[n];
-    } else if (n < 20) {
-      str = lessThan20[n - 10];
-    } else if (n < 100) {
-      str = lessThan100[n / 10] + " " + digits[n % 10];
-    } else if (n < 1000) {
-      str = convert(n / 100) + " Hundred " + convert(n % 100);
-    } else if (n < 1000000) {
-      str = convert(n / 1000) + " Thousand " + convert(n % 1000);
-    } else if (n < 1000000000) {
-      str = convert(n / 1000000) + " Million " + convert(n % 1000000);
-    } else str = convert(n / 1000000000) + " Billion " + convert(n % 1000000000);
-    return str.trim();
+  
+  private String helper(int num) {
+    String result = "";
+    
+    if (num < 10) {
+      result = lessThanTen[num];
+    } else if (num < 20) {
+      result = lessThenTwenty[num - 10];
+    } else if (num < 100) {
+      result = lessThenHundred[num / 10] + " " + helper(num % 10);
+    } else if (num < 1000) {
+      result = helper(num / 100) + " Hundred " + helper(num % 100);
+    } else if (num < 1000000) {
+      result = helper(num / 1000) + " Thousand " + helper(num % 1000);
+    } else if (num < 1000000000) {
+      result = helper(num / 1000000) + " Million " + helper(num % 1000000);
+    } else {
+      result = helper(num / 1000000000) + " Billion " + helper(num % 1000000000);
+    }
+    
+    return result.trim();
   }
 }
