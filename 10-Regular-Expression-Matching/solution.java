22b5847226d760ceb457b889a84ea8b14297e992
Xu ZHANG <laonawuli@126.com>
2016-06-16 00:06:32 -0700

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,27 +1,30 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
+        return isMatch(s, p, 0, s.length() - 1, 0, p.length() - 1);
+    }
+    
+    private boolean isMatch(String s, String p, int sL, int sR, int pL, int pR) {
+        int sLen = sR - sL + 1;
+        int pLen = pR - pL + 1;
+        if (pLen == 0) {
+            return sLen == 0;
         }
-        if (p.length() == 0) {
-            return s.length() == 0;
-        }
-        if (p.length() == 1 || p.charAt(1) != '*') {
-            if (s.length() == 0) {
+        if (pLen == 1 || p.charAt(pL + 1) != '*') {
+            if (sLen == 0) {
                 return false;
             }
-            if (match(s, p, 0, 0)) {
-                return isMatch(s.substring(1), p.substring(1));
+            if (match(s, p, sL, pL)) {
+                return isMatch(s, p, sL + 1, sR, pL + 1, pR);
             }
             return false;
         }
-        if (p.charAt(1) == '*') {
-            if (isMatch(s, p.substring(2))) {
+        if (p.charAt(pL + 1) == '*') {
+            if (isMatch(s, p, sL, sR, pL + 2, pR)) {
                 return true;
             }
             int i = 0;
-            while (i < s.length() && match(s, p, i, 0)) {
-                if (isMatch(s.substring(i + 1), p.substring(2))) {
+            while (i < sLen && match(s, p, sL + i, pL)) {
+                if (isMatch(s, p, sL + i + 1, sR, pL + 2, pR)) {
                     return true;
                 }
                 i++;
