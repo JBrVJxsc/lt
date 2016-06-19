b3bc17eb643708b1f21557fdc7e558288310989e
Xu ZHANG <laonawuli@126.com>
2016-06-19 10:32:57 -0700

    [33. Search in Rotated Sorted Array][Accepted]

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -7,23 +7,36 @@ public class Solution {
     if (left > right) {
       return -1;
     }
+    
     int mid = left + (right - left) / 2;
     if (nums[mid] == target) {
       return mid;
-    } else {
-      if (nums[left] <= nums[mid]) {
-        if (nums[left] <= target && target < nums[mid]) {
-          return binary(nums, target, left, mid - 1);
-        } else {
-          return binary(nums, target, mid + 1, right);
-        }
+    }
+    
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        return binary(nums, target, left, mid - 1);
       } else {
-        if (nums[mid] < target && target <= nums[right]) {
-          return binary(nums, target, mid + 1, right);
-        } else {
-          return binary(nums, target, left, mid - 1);
-        }        
+        return binary(nums, target, mid + 1, right);
       }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        return binary(nums, target, mid + 1, right);
+      } else {
+        return binary(nums, target, left, mid - 1);        
+      }      
     }
   }
 }
+
+
+
+
+// 567891234
+  
+// ^   ^
+
+// 51238
+
+// 1. smallest one will ALWAYS be right of the largest one.
+// 2. 
