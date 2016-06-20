db9dda4a8ae5d887ba1b5ab229ad3f42da28ae2b
Xu ZHANG <laonawuli@126.com>
2016-06-20 09:11:09 -0700

    [100. Same Tree][Accepted]

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -15,9 +15,6 @@ public class Solution {
         if (p == null || q == null || p.val != q.val) {
             return false;
         }
-        if (!isSameTree(p.left, q.left)) {
-            return false;
-        }
-        return isSameTree(p.right, q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
