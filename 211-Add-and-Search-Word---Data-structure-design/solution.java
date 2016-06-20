a977bf482ae648b0423e160a61b9f80726e67953
Xu ZHANG <laonawuli@126.com>
2016-06-20 08:52:32 -0700

    [211. Add and Search Word - Data structure design][Accepted]

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -1,47 +1,54 @@
 public class WordDictionary {
-  class Node {
-    Node[] nodes = new Node[26];
-    boolean isWord = false;
-  }
-  
-  private Node root = new Node();
-  
-  public void addWord(String word) {
-    Node next = root;
-    for (char c : word.toCharArray()) {
-      int pos = c - 'a';
-      if (next.nodes[pos] == null) {
-        next.nodes[pos] = new Node();
-      }
-      next = next.nodes[pos];
+
+    class Node {
+        Node[] nodes = new Node[26];
+        boolean isWord = false;
     }
-    next.isWord = true;
-  }
-  
-  public boolean search(String word) {
-    return dfs(root, word, 0);
-  }
-  
-  private boolean dfs(Node node, String word, int index) {
-    if (node == null) {
-      return false;
-    }
-    if (index == word.length()) {
-      return node.isWord;
-    }
-    char c = word.charAt(index);
-    if (c == '.') {
-      for (Node n : node.nodes) {
-        if (dfs(n, word, index + 1)) {
-          return true;
+    
+    private Node root = new Node();
+    
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        Node node = root;
+        for (char c : word.toCharArray()) {
+            int pos = c - 'a';
+            if (node.nodes[pos] == null) {
+                node.nodes[pos] = new Node();
+            }
+            node = node.nodes[pos];
         }
-      }
-    } else {
-      int pos = c - 'a';
-      return dfs(node.nodes[pos], word, index + 1);
+        node.isWord = true;
     }
-    return false;
-  }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+    
+    private boolean dfs(String word, int index, Node node) {
+        if (node == null) {
+            return false;
+        }
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (Node n : node.nodes) {
+                if (dfs(word, index + 1, n)) {
+                    return true;
+                }
+            }
+        } else {
+            int pos = c - 'a';
+            if (node.nodes[pos] == null) {
+                return false;
+            }
+            return dfs(word, index + 1, node.nodes[pos]);
+        }
+        return false;
+    }
 }
 
 // Your WordDictionary object will be instantiated and called as such:
