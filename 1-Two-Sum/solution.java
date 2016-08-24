3262c540a80ac2bee94af2c81f85d8cd3c8a1f2c
Xu ZHANG <laonawuli@126.com>
2016-08-23 22:21:21 -0700

    [1. Two Sum][Accepted]

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
-  public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-      map.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = map.get(target - nums[i]);
+            if (index != null && index != i) {
+                return new int[]{i, index};
+            }
+        }
+        
+        return null;
     }
-    
-    for (int i = 0; i < nums.length; i++) {
-      Integer index = map.get(target - nums[i]);
-      if (index == null || index == i) {
-        continue;
-      }
-      return new int[]{Math.min(i, index), Math.max(i, index)};
-    }
-    
-    return null;
-  }
 }
