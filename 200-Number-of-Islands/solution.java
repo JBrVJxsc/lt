277e5305a15e4ee11d2818c1218038a8ad77b725
Xu ZHANG <laonawuli@126.com>
2016-08-24 08:47:03 -0700

    [200. Number of Islands][Accepted]

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,37 +1,35 @@
 public class Solution {
-  public int numIslands(char[][] grid) {
-    if (grid == null || grid.length == 0 || grid[0].length == 0) {
-      return 0;
-    }
-    
-    int row = grid.length;
-    int col = grid[0].length;
-    int num = 0;
-    boolean[][] visited = new boolean[row][col];
-    
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
-        if (dfs(grid, i, j, row, col, visited) > 0) {
-          num++;
+    public int numIslands(char[][] grid) {
+        int row = grid.length;
+        if (row == 0) {
+            return 0;
         }
-      }
+        int col = grid[0].length;
+        int counter = 0;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (dfs(grid, i, j, row, col) > 0) {
+                    counter++;
+                }
+            }
+        }
+        
+        return counter;
     }
     
-    return num;
-  }
-  
-  private int dfs(char[][] grid, int x, int y, int row, int col, boolean[][] visited) {
-    if (x < 0 || x >= row || y < 0 || y >= col || visited[x][y] || grid[x][y] != '1') {
-      return 0;
+    private int dfs(char[][] grid, int i, int j, int row, int col) {
+        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1') {
+            return 0;
+        }
+        grid[i][j] = '#';
+        
+        int counter = 1;
+        counter += dfs(grid, i - 1, j, row, col);
+        counter += dfs(grid, i + 1, j, row, col);
+        counter += dfs(grid, i, j - 1, row, col);
+        counter += dfs(grid, i, j + 1, row, col);
+        
+        return counter;
     }
-    
-    visited[x][y] = true;
-    int size = 1;
-    size += dfs(grid, x - 1, y, row, col, visited);
-    size += dfs(grid, x + 1, y, row, col, visited);
-    size += dfs(grid, x, y - 1, row, col, visited);
-    size += dfs(grid, x, y + 1, row, col, visited);
-    
-    return size;
-  }
 }
