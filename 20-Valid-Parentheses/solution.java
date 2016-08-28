8dab78d493a2a0ab257104c64f87dde376285c7c
Xu ZHANG <laonawuli@126.com>
2016-08-28 10:45:32 -0700

    [20. Valid Parentheses][Accepted]

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,21 +1,25 @@
 public class Solution {
-  public boolean isValid(String s) {
-    Stack<Character> stack = new Stack<>();
-    for (char c : s.toCharArray()) {
-      if (stack.isEmpty()) {
-        stack.push(c);
-      } else {
-        if (c == '{' || c =='[' || c == '(') {
-          stack.push(c);
-        } else {
-          if (c == '}' && stack.peek() == '{' || c == ')' && stack.peek() == '(' || c == ']' && stack.peek() == '[') {
-            stack.pop();
-          } else {
-            stack.push(c);
-          }
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
         }
-      }
-    } 
-    return stack.isEmpty();
-  }
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
 }
