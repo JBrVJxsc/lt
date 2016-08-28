3b5dbcd470b673381d2eb80a7d1eab7392058fc4
Xu ZHANG <laonawuli@126.com>
2016-08-28 13:32:58 -0700

    [297. Serialize and Deserialize Binary Tree][Accepted]

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.java
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.java
@@ -9,58 +9,56 @@
  */
 public class Codec {
 
-  // Encodes a tree to a single string.
-  public String serialize(TreeNode root) {
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    StringBuilder sb = new StringBuilder();
-    
-    while (!queue.isEmpty()) {
-      TreeNode node = queue.remove();
-      if (node == null) {
-        sb.append('*').append('#');
-        continue;
-      }
-      sb.append(node.val).append('#');
-      queue.add(node.left);
-      queue.add(node.right);
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {        
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            if (node == null) {
+                sb.append('#');
+                sb.append('~');
+            } else {
+                sb.append(node.val);
+                sb.append('~');
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        
+        return sb.toString();
     }
-    
-    return sb.toString();
-  }
 
-  // Decodes your encoded data to tree.
-  public TreeNode deserialize(String data) {
-    if (data.equals("*#")) {
-      return null;
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.charAt(0) == '#') {
+            return null;
+        }
+        
+        String[] strs = data.split("~");
+        TreeNode root = new TreeNode(Integer.valueOf(strs[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            String temp = strs[i++];
+            if (!temp.equals("#")) {
+                node.left = new TreeNode(Integer.valueOf(temp));
+                queue.add(node.left);
+            }
+            temp = strs[i++];
+            if (!temp.equals("#")) {
+                node.right = new TreeNode(Integer.valueOf(temp));
+                queue.add(node.right);
+            }            
+        }
+        
+        return root;
     }
-    
-    String[] strs = data.split("#");
-    TreeNode root = new TreeNode(Integer.valueOf(strs[0]));
-    int pos = 1;
-    
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    
-    while (!queue.isEmpty()) {
-      TreeNode node = queue.remove();
-      if (pos == strs.length) {
-        break;
-      }
-      String val = strs[pos++];
-      if (!val.equals("*")) {
-        node.left = new TreeNode(Integer.valueOf(val));
-        queue.add(node.left);
-      }
-      val = strs[pos++];
-      if (!val.equals("*")) {
-        node.right = new TreeNode(Integer.valueOf(val));
-        queue.add(node.right);
-      }
-    }
-    
-    return root;
-  }
 }
 
 // Your Codec object will be instantiated and called as such:
