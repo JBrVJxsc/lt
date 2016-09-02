1269207aff37fbf72995bc3edf68a2eaba2e43c6
Xu ZHANG <laonawuli@126.com>
2016-09-01 22:39:23 -0700

    [139. Word Break][Accepted]

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,31 +1,30 @@
 public class Solution {
-  public boolean wordBreak(String s, Set<String> dict) {
-    boolean[] visited = new boolean[s.length()];
-    boolean[] result = new boolean[s.length()];
-    return dfs(s, dict, 0, visited, result);
-  }
-  
-  private boolean dfs(String s, Set<String> dict, int index, boolean[] visited, boolean[] result) {
-    if (index == s.length()) {
-      return true;
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null) {
+            return false;
+        }
+        boolean[] result = new boolean[s.length()];
+        boolean[] visited = new boolean[s.length()];
+        return dfs(s, dict, 0, result, visited);
     }
     
-    if (visited[index]) {
-      return result[index];
+    private boolean dfs(String s, Set<String> dict, int index, boolean[] result, boolean[] visited) {
+        if (index == s.length()) {
+            return true;
+        }
+        
+        if (visited[index]) {
+            return result[index];
+        }
+        visited[index] = true;
+        
+        for (int i = index; i < s.length(); i++) {
+            String sub = s.substring(index, i + 1);
+            if (dict.contains(sub) && dfs(s, dict, i + 1, result, visited)) {
+                result[index] = true;
+            }
+        }
+        
+        return result[index];
     }
-    visited[index] = true;
-    
-    for (int i = index; i < s.length(); i++) {
-      String sub = s.substring(index, i + 1);
-      if (!dict.contains(sub)) {
-        continue;
-      }
-      if (dfs(s, dict, i + 1, visited, result)) {
-        result[index] = true;
-        break;
-      }
-    }
-    
-    return result[index];
-  }
 }
