7931e676345ffe3d01ee1ca5d8a9d22b05809e3f
Xu ZHANG <laonawuli@126.com>
2016-09-02 07:53:33 -0700

    [15. 3Sum][Accepted]

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,42 +1,43 @@
 public class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> lists = new ArrayList<>();
-    if (nums == null || nums.length < 3) {
-      return lists;
-    }
-    
-    Arrays.sort(nums);
-    
-    for (int i = 0; i < nums.length - 2; i++) {
-      if (i != 0 && nums[i] == nums[i - 1]) {
-        continue;
-      }
-      int l = i + 1;
-      int r = nums.length - 1;
-      while (l < r) {
-        int sum = nums[l] + nums[r];
-        if (sum == -nums[i]) {
-          List<Integer> list = new ArrayList<>();
-          list.add(nums[i]);
-          list.add(nums[l]);
-          list.add(nums[r]);          
-          lists.add(list);
-          l++;
-          r--;
-          while (l < r && nums[l] == nums[l - 1]) {
-            l++;
-          }
-          while (l < r && nums[r] == nums[r + 1]) {
-            r--;
-          }          
-        } else if (sum < -nums[i]) {
-          l++;
-        } else {
-          r--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return lists;
         }
-      }
+        
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int target = -nums[i];
+                int l = i + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[l] + nums[r];
+                    if (sum == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[l]);
+                        list.add(nums[r]);
+                        lists.add(list);
+                        
+                        l++;
+                        r--;
+                        
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r + 1]) {
+                            r--;
+                        }
+                    } else if (sum < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        
+        return lists;
     }
-    
-    return lists;
-  }
 }
