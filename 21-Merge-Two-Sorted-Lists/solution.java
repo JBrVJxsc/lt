da2a8df0fa769860fb0b63e59ba00c9d61c26aca
Xu ZHANG <laonawuli@126.com>
2016-09-02 20:23:09 -0700

    [21. Merge Two Sorted Lists][Accepted]

--- 21-Merge-Two-Sorted-Lists/solution.java
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -7,35 +7,35 @@
  * }
  */
 public class Solution {
-  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    if (l1 == null) {
-      return l2;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode next = dummy;
+        
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                next.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                next.next = l1;
+                l1 = l1.next;
+            } else if (l1.val < l2.val) {
+                next.next = l1;
+                l1 = l1.next;
+            } else {
+                next.next = l2;
+                l2 = l2.next;                
+            }
+            next.next.next = null;
+            next = next.next;
+        }
+        
+        return dummy.next;
     }
-    if (l2 == null) {
-      return l1;
-    }
-    
-    ListNode dummy = new ListNode(-1);
-    ListNode next = dummy;
-    
-    while (l1 != null || l2 != null) {
-      if (l1 == null) {
-        next.next = l2;
-        l2 = l2.next;
-      } else if (l2 == null) {
-        next.next = l1;
-        l1 = l1.next;
-      } else if (l1.val < l2.val) {
-        next.next = l1;
-        l1 = l1.next;
-      } else {
-        next.next = l2;
-        l2 = l2.next;        
-      }
-      next.next.next = null;
-      next = next.next;
-    }
-    
-    return dummy.next;
-  }
 }
