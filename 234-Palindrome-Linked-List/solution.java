3fdb76d0b95704e0d5f79a6cf74d0e2861716669
Xu ZHANG <laonawuli@126.com>
2016-09-02 20:58:40 -0700

    [234. Palindrome Linked List][Accepted]

--- 234-Palindrome-Linked-List/solution.java
+++ 234-Palindrome-Linked-List/solution.java
@@ -7,57 +7,52 @@
  * }
  */
 public class Solution {
-  public boolean isPalindrome(ListNode head) {
-    if (head == null) {
-      return true;
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        int len = 0;
+        ListNode next = head;
+        while (next != null) {
+            len++;
+            next = next.next;
+        }
+        
+        int mid = len - len / 2;
+        next = head;
+        
+        while (mid-- > 0) {
+            next = next.next;
+        }
+        
+        next = reverse(next);
+        while (next != null) {
+            if (next.val != head.val) {
+                return false;
+            }
+            next = next.next;
+            head = head.next;
+        }
+        
+        return true;
     }
     
-    int count = 0;
-    ListNode next = head;
-    while (next != null) {
-      count++;
-      next = next.next;
+    private ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        while (head.next != null) {
+            ListNode next = head.next;
+            head.next = head.next.next;
+            next.next = dummy.next;
+            dummy.next = next;
+        }
+        
+        return dummy.next;
     }
-    
-    int mid = count / 2;
-    mid += count % 2;
-    
-    int counter = 1;
-    next = head;
-    
-    while (counter != mid) {
-      counter++;
-      next = next.next;
-    }
-    
-    ListNode reversed = reverse(next.next);
-    
-    while (reversed != null) {
-      if (reversed.val != head.val) {
-        return false;
-      }
-      reversed = reversed.next;
-      head = head.next;
-    }
-    
-    return true;
-  }
-  
-  private ListNode reverse(ListNode head) {
-    if (head == null) {
-      return null;
-    }
-    
-    ListNode dummy = new ListNode(-1);
-    dummy.next = head;
-    
-    while (head.next != null) {
-      ListNode next = head.next;
-      head.next = head.next.next;
-      next.next = dummy.next;
-      dummy.next = next;
-    }
-    
-    return dummy.next;
-  }
 }
