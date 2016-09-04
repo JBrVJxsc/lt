79d08e639ad84eb7e1aeab7afb74c14ac1e595d6
Xu ZHANG <laonawuli@126.com>
2016-09-04 12:52:48 -0700

    [186. Reverse Words in a String II][Accepted]

--- 186-Reverse-Words-in-a-String-II/solution.java
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -6,11 +6,10 @@ public class Solution {
         reverse(s, 0, s.length - 1);
         int l = 0;
         for (int i = 0; i < s.length; i++) {
-            if (s[i] != ' ') {
-                continue;
+            if (s[i] == ' ') {
+                reverse(s, l, i - 1);
+                l = i + 1;
             }
-            reverse(s, l, i - 1);
-            l = i + 1;
         }
         reverse(s, l, s.length - 1);
     }
