aacfa0204839162589983d037b00b9ca71a30c04
Xu ZHANG <laonawuli@126.com>
2016-09-06 23:28:00 -0700

    [33. Search in Rotated Sorted Array][Accepted]

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,42 +1,27 @@
 public class Solution {
-  public int search(int[] nums, int target) {
-    return binary(nums, target, 0, nums.length - 1);
-  }
-  
-  private int binary(int[] nums, int target, int left, int right) {
-    if (left > right) {
-      return -1;
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] <= nums[r]) {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }                
+            }
+        }
+        
+        return -1;
     }
-    
-    int mid = left + (right - left) / 2;
-    if (nums[mid] == target) {
-      return mid;
-    }
-    
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) {
-        return binary(nums, target, left, mid - 1);
-      } else {
-        return binary(nums, target, mid + 1, right);
-      }
-    } else {
-      if (nums[mid] < target && target <= nums[right]) {
-        return binary(nums, target, mid + 1, right);
-      } else {
-        return binary(nums, target, left, mid - 1);        
-      }      
-    }
-  }
 }
-
-
-
-
-// 567891234
-  
-// ^   ^
-
-// 51238
-
-// 1. smallest one will ALWAYS be right of the largest one.
-// 2. 
