187c3910e34d2098ac7982ee5d921716a2d77f8c
Xu ZHANG <laonawuli@126.com>
2016-09-08 07:30:01 -0700

    [265. Paint House II][Accepted]

--- 265-Paint-House-II/solution.java
+++ 265-Paint-House-II/solution.java
@@ -1,34 +1,38 @@
 public class Solution {
-  public int minCostII(int[][] costs) {
-    if (costs == null || costs.length == 0) {
-      return 0;
-    }
-    
-    for (int i = 1; i < costs.length; i++) {
-      int[] mins = minTwo(costs[i - 1]);
-      for (int j = 0; j < costs[i].length; j++) {
-        if (costs[i - 1][j] == mins[0]) {
-          costs[i][j] += mins[1];
-        } else {
-          costs[i][j] += mins[0];
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
         }
-      }
+        int k = costs[0].length;
+        
+        for (int i = 1; i < costs.length; i++) {
+            int[] min = min2(costs[i - 1]);
+            for (int j = 0; j < k; j++) {
+                costs[i][j] += costs[i - 1][j] == min[0] ? min[1] : min[0];
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < k; i++) {
+            min = Math.min(min, costs[costs.length - 1][i]);
+        }
+        
+        return min;
     }
-    
-    return minTwo(costs[costs.length - 1])[0];
-  }
   
-  private int[] minTwo(int[] nums) {
-    int a = Integer.MAX_VALUE;
-    int b = Integer.MAX_VALUE;
-    for (int n : nums) {
-      if (n < a) {
-        b = a;
-        a = n;
-      } else if (n < b) {
-        b = n;
-      }
+    private int[] min2(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            if (num < a) {
+                b = a;
+                a = num;
+            } else if (num < b) {
+                b = num;
+            }
+        }
+                
+        return new int[]{a, b};
     }
-    return new int[]{a, b};
-  }
 }
