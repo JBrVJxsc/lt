8238520b0260ac66a85be1b6194c7e0d5b07ee4d
Xu ZHANG <laonawuli@126.com>
2016-09-09 20:29:13 -0700

    [187. Repeated DNA Sequences][Accepted]

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,41 +1,37 @@
 public class Solution {
-public List<String> findRepeatedDnaSequences(String s) {
-	List<String> result = new ArrayList<String>();
- 
-	int len = s.length();
-	if (len < 10) {
-		return result;
-	}
- 
-	Map<Character, Integer> map = new HashMap<Character, Integer>();
-	map.put('A', 0);
-	map.put('C', 1);
-	map.put('G', 2);
-	map.put('T', 3);
- 
-	Set<Integer> temp = new HashSet<Integer>();
-	Set<Integer> added = new HashSet<Integer>();
- 
-	int hash = 0;
-	for (int i = 0; i < len; i++) {
-		if (i < 9) {
-			//each ACGT fit 2 bits, so left shift 2
-			hash = (hash << 2) + map.get(s.charAt(i)); 
-		} else {
-			hash = (hash << 2) + map.get(s.charAt(i));
-			//make length of hash to be 20
-			hash = hash &  (1 << 20) - 1; 
- 
-			if (temp.contains(hash) && !added.contains(hash)) {
-				result.add(s.substring(i - 9, i + 1));
-				added.add(hash); //track added
-			} else {
-				temp.add(hash);
-			}
-		}
- 
-	}
- 
-	return result;
-}
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> list = new ArrayList<>();
+        if (s == null || s.length() < 10) {
+            return list;
+        }
+        
+        Set<Integer> first = new HashSet<>();
+        Set<Integer> second = new HashSet<>();
+        int num = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int n = 0;
+            if (c == 'C') {
+                n = 1;
+            } else if (c == 'G') {
+                n = 2;
+            } else if (c == 'T') {
+                n = 3;
+            }
+            
+            num = (num << 2) + n;
+            if (i >= 9) {
+                num &= (1 << 20) - 1;
+                if (first.contains(num) && !second.contains(num)) {
+                    list.add(s.substring(i - 9, i + 1));
+                    second.add(num);
+                } else {
+                    first.add(num);
+                }
+            }
+        }
+        
+        return list;
+    }
 }
