e5a86fcdb19254654d7e7233c3e25bde3f9d96d5
Xu ZHANG <laonawuli@126.com>
2016-09-11 10:18:05 -0700

    [65. Valid Number][Accepted]

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -1,3 +1,38 @@
+// Integer:
+// 9
+
+// Decimal:
+// 9.9  
+// 9.
+// .9
+// Rules:
+// 1. Dot cannot be more than 1.
+// 2. If dot is the last char, there must be a number before it.
+
+// Signed:
+// +(Integer)
+// -(Integer)
+// +(Decimal)
+// -(Decimal)
+
+// Unsigned:
+// Integer
+// Decimal
+
+// Exponential:
+// (Signed)e(Integer)
+// (Signed)e+(Integer)
+// (Signed)e-(Integer)
+// (Unsigned)e(Integer)
+// (Unsigned)e+(Integer)
+// (Unsigned)e-(Integer)
+// Rules:
+// 1. e cannot be more than 1, and e cannot be the last char.
+// 2. There should be numbers before e.
+// 3. +, - cannot be more than 2, and cannot be the last char.
+// 4. If +, - appears in the middle, then there must be an e before it.
+
+
 public class Solution {
     public boolean isNumber(String s) {
         if (s == null) {
@@ -5,16 +40,19 @@ public class Solution {
         }
         
         s = s.trim();
+        s = s.toLowerCase();
+        
         int len = s.length();
         if (len == 0) {
             return false;
         }
         
-        int sign = 0;
-        boolean hasE = false;
-        boolean hasNum = false;
-        boolean hasDot = false;
+        int signCount = 0;
+        int eCount = 0;
+        int digitCount = 0;
+        int dotCount = 0;
         
+        // Do check.
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
             
@@ -23,47 +61,54 @@ public class Solution {
             }
             
             if (isDigit(c)) {
-                hasNum = true;
-            }
-            
-            if (isE(c)) {
-                if (hasE || !hasNum) {
-                    return false;
-                }
-                if (i == len - 1) {
-                    return false;
-                }
-                hasE = true;
-            }
-            
-            if (isDot(c)) {
-                if (hasDot || hasE) {
-                    return false;
-                }
-                if (i == len - 1 && !hasNum) {
-                    return false;
-                }
-                hasDot = true;
+                digitCount++;
             }
             
             if (isSign(c)) {
-                if (sign == 2) {
+                if (signCount == 2) {
+                    return false;
+                }
+                if (i != 0 && eCount == 0) {
                     return false;
                 }
                 if (i == len - 1) {
                     return false;
                 }
-                if (i > 0 && !hasE) {
+                signCount++;
+            }
+            
+            if (c == 'e') {
+                if (eCount == 1) {
                     return false;
                 }
-                sign++;
+                if (digitCount == 0) {
+                    return false;
+                }
+                if (i == len - 1) {
+                    return false;
+                }
+                eCount++;
+            }
+            
+            if (c == '.') {
+                if (dotCount == 1) {
+                    return false;
+                }
+                if (i == len - 1 && digitCount == 0) {
+                    return false;
+                }
+                if (eCount > 0) {
+                    return false;
+                }
+                dotCount++;
             }
         }
+        
         return true;
     }
     
     private boolean isValid(char c) {
-        return isDot(c) || isSign(c) || isE(c) || isDigit(c);
+        return c == '.' || c == 'e' || isDigit(c) || isSign(c);
     }
     
     private boolean isDigit(char c) {
@@ -72,13 +117,5 @@ public class Solution {
     
     private boolean isSign(char c) {
         return c == '+' || c == '-';
-    }
-    
-    private boolean isE(char c) {
-        return c == 'e' || c == 'E';
-    }
-    
-    private boolean isDot(char c) {
-        return c == '.';
     }
 }
