d009bf0a1ac44540dfc6fda9d270ea44ed76afea
Xu ZHANG <laonawuli@126.com>
2016-09-11 17:40:35 -0700

    [69. Sqrt(x)][Accepted]

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -1,22 +1,29 @@
 public class Solution {
     public int mySqrt(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("x cannot be less than 0.");
+        }
         if (x < 2) {
             return x;
         }
-        return binary(1, x, x);
+        return binary(x, 1, x);
     }
     
-    private int binary(int l, int r, int x) {
+    private int binary(int x, int l, int r) {
         int m = l + (r - l) / 2;
         if (m == x / m) {
             return m;
-        }
-        if (m < x / m) {
-            if (m + 1 > x / (m + 1)) {
+        } else if (m > x / m) {
+            return binary(x, l, m - 1);
+        } else {
+            m += 1;
+            if (m == x / m) {
                 return m;
+            } else if (m > x / m) {
+                return m - 1;
+            } else {
+                return binary(x, m, r);
             }
-            return binary(m + 1, r, x);
         }
-        return binary(l, m - 1, x);
     }
 }
