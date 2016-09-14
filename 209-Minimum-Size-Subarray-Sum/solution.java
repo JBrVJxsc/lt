26f12a9877b623d25843da8875ecb5a8b1fd5ec8
Xu ZHANG <laonawuli@126.com>
2016-09-14 00:11:15 -0700

    [209. Minimum Size Subarray Sum][Accepted]

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,28 +1,33 @@
 public class Solution {
-  public int minSubArrayLen(int s, int[] nums) {
-    int l = 0;
-    int r = 0;
-    int sum = 0;
-    int min = 0;
-    
-    while (r < nums.length) {
-      while (r < nums.length && sum < s) {
-        sum += nums[r++];
-      }
-      
-      while (l < nums.length && sum >= s) {
-        sum -= nums[l++];
-        if (sum < s) {
-          int len = r - l + 1;
-          if (min == 0) {
-            min = len;
-          } else {
-            min = Math.min(min, len);
-          }
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-      }
+        
+        int min = 0;
+        int sum = 0;
+        int l = 0;
+        int r = 0;
+        
+        while (r < nums.length) {
+            while (r < nums.length && sum < s) {
+                sum += nums[r++];
+            }
+            
+            if (sum >= s) {
+                if (min == 0) {
+                    min = r - l;
+                } else {
+                    min = Math.min(min, r - l);
+                }
+            }
+            
+            while (sum >= s) {
+                sum -= nums[l++];
+                min = Math.min(min, r - l + 1);
+            }
+        }
+        
+        return min;
     }
-    
-    return min;
-  }
 }
