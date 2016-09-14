21db909e86a6fd2f35d7156833cc714d3db62919
Xu ZHANG <laonawuli@126.com>
2016-09-13 20:01:20 -0700

    [65. Valid Number][Accepted]

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -1,40 +1,3 @@
-// Integer:
-// 9
-
-// Decimal:
-// 9.9  
-// 9.
-// .9
-
-// Signed:
-// +(Integer)
-// -(Integer)
-// +(Decimal)
-// -(Decimal)
-
-// Unsigned:
-// Integer
-// Decimal
-
-// Exponential:
-// (Signed)e(Integer)
-// (Signed)e+(Integer)
-// (Signed)e-(Integer)
-// (Unsigned)e(Integer)
-// (Unsigned)e+(Integer)
-// (Unsigned)e-(Integer)
-
-// Rules:
-// 1. Dot cannot be more than 1.
-// 2. If dot is the last char, then there must be a digit before it.
-// 3. Dot cannot be after the e.
-// 4. e cannot be more than 1.
-// 5. e cannot be the last char.
-// 6. There must be digit before e.
-// 7. +, - cannot be more than 2.
-// 8. +, - cannot be the last char.
-// 9. If +, - is in the middle of the str, then there must be a e before it.
-
 public class Solution {
     public boolean isNumber(String s) {
         if (s == null) {
@@ -42,18 +5,17 @@ public class Solution {
         }
         
         s = s.trim();
-        s = s.toLowerCase();        
+        s = s.toLowerCase();
         int len = s.length();
         if (len == 0) {
             return false;
         }
         
-        int signCount = 0;
-        int eCount = 0;
-        int digitCount = 0;
-        int dotCount = 0;
+        int numOfDigit = 0;
+        int numOfE = 0;
+        int numOfDot = 0;
+        int numOfSign = 0;
         
-        // Do check.
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
             
@@ -62,54 +24,54 @@ public class Solution {
             }
             
             if (isDigit(c)) {
-                digitCount++;
+                numOfDigit++;
             }
             
             if (c == '.') {
-                if (dotCount == 1) {
+                numOfDot++;
+                if (numOfDot > 1) {
                     return false;
                 }
-                if (i == len - 1 && digitCount == 0) {
+                if (i == len - 1 && numOfDigit == 0) {
                     return false;
                 }
-                if (eCount > 0) {
+                if (numOfE > 0) {
                     return false;
                 }
-                dotCount++;
-            }            
+            }
             
             if (c == 'e') {
-                if (eCount == 1) {
+                numOfE++;
+                if (numOfE > 1) {
                     return false;
                 }
                 if (i == len - 1) {
                     return false;
                 }
-                if (digitCount == 0) {
+                if (numOfDigit == 0) {
                     return false;
                 }
-                eCount++;
             }
             
             if (isSign(c)) {
-                if (signCount == 2) {
+                numOfSign++;
+                if (numOfSign > 2) {
                     return false;
                 }
                 if (i == len - 1) {
                     return false;
                 }
-                if (i != 0 && eCount == 0) {
+                if (i != 0 && numOfE == 0) {
                     return false;
                 }
-                signCount++;
-            }            
+            }
         }
         
         return true;
     }
     
     private boolean isValid(char c) {
-        return c == '.' || c == 'e' || isDigit(c) || isSign(c);
+        return c == 'e' || c == '.' || isDigit(c) || isSign(c);
     }
     
     private boolean isDigit(char c) {
