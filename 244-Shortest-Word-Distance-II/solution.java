aade910de9cd8e92f64a0519957eff8b169c7ea5
Xu ZHANG <laonawuli@126.com>
2016-09-14 23:52:26 -0700

    [244. Shortest Word Distance II][Accepted]

--- 244-Shortest-Word-Distance-II/solution.java
+++ 244-Shortest-Word-Distance-II/solution.java
@@ -1,6 +1,6 @@
 public class WordDistance {
 
-    private Map<String, List<Integer>> map = new HashMap<>();
+    Map<String, ArrayList<Integer>> map = new HashMap<>();
     
     public WordDistance(String[] words) {
         for (int i = 0; i < words.length; i++) {
@@ -12,11 +12,13 @@ public class WordDistance {
     }
 
     public int shortest(String word1, String word2) {
-        int min = Integer.MAX_VALUE;
-        List<Integer> l1 = map.get(word1);
-        List<Integer> l2 = map.get(word2);
         int i = 0;
         int j = 0;
+        int min = Integer.MAX_VALUE;
+        
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
+        
         while (i < l1.size() && j < l2.size()) {
             min = Math.min(min, Math.abs(l1.get(i) - l2.get(j)));
             if (l1.get(i) < l2.get(j)) {
@@ -25,6 +27,7 @@ public class WordDistance {
                 j++;
             }
         }
+        
         return min;
     }
 }
