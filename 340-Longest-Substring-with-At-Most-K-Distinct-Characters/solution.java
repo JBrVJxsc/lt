cfa1424c061701c95df9e81dd3280e909829b8c3
Xu ZHANG <laonawuli@126.com>
2016-09-14 23:22:12 -0700

    [340. Longest Substring with At Most K Distinct Characters][Accepted]

---
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (s == null || s.length() == 0 || k == 0) {
+            return 0;
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;        
+        int l = 0;
+        int r = 0;
+        while (r < s.length()) {
+            while (r < s.length() && map.size() < k) {
+                char c = s.charAt(r++);                
+                int count = map.getOrDefault(c, 0) + 1;
+                map.put(c, count);
+            }
+            
+            while (r < s.length() && map.size() == k && map.get(s.charAt(r)) != null) {
+                char c = s.charAt(r++);                
+                int count = map.getOrDefault(c, 0) + 1;
+                map.put(c, count);
+            }            
+            
+            max = Math.max(max, r - l);
+            
+            while (map.size() == k) {
+                char c = s.charAt(l++);
+                int count = map.get(c) - 1;
+                if (count == 0) {
+                    map.remove(c);
+                } else {
+                    map.put(c, count);
+                }
+            }            
+        }
+        
+        return max;
+    }
+}
