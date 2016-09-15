1f72adf112010dd06fb7a791c376885d4f0bf05d
Xu ZHANG <laonawuli@126.com>
2016-09-15 00:10:45 -0700

    [65. Valid Number][Accepted]

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -12,11 +12,11 @@ public class Solution {
         }
         
         int numOfDigit = 0;
-        int numOfE = 0;
         int numOfDot = 0;
+        int numOfE = 0;
         int numOfSign = 0;
         
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             
             if (!isValid(c)) {
@@ -27,22 +27,8 @@ public class Solution {
                 numOfDigit++;
             }
             
-            if (c == '.') {
-                numOfDot++;
-                if (numOfDot > 1) {
-                    return false;
-                }
-                if (i == len - 1 && numOfDigit == 0) {
-                    return false;
-                }
-                if (numOfE > 0) {
-                    return false;
-                }
-            }
-            
             if (c == 'e') {
-                numOfE++;
-                if (numOfE > 1) {
+                if (numOfE == 1) {
                     return false;
                 }
                 if (i == len - 1) {
@@ -51,11 +37,24 @@ public class Solution {
                 if (numOfDigit == 0) {
                     return false;
                 }
+                numOfE++;
             }
             
-            if (isSign(c)) {
-                numOfSign++;
-                if (numOfSign > 2) {
+            if (c == '.') {
+                if (numOfDot == 1) {
+                    return false;
+                }
+                if (i == len - 1 && numOfDigit == 0) {
+                    return false;
+                }                
+                if (numOfE > 0) {
+                    return false;
+                }
+                numOfDot++;
+            }
+            
+            if (c == '+' || c == '-') {
+                if (numOfSign == 2) {
                     return false;
                 }
                 if (i == len - 1) {
@@ -64,6 +63,7 @@ public class Solution {
                 if (i != 0 && numOfE == 0) {
                     return false;
                 }
+                numOfSign++;
             }
         }
         
@@ -71,14 +71,10 @@ public class Solution {
     }
     
     private boolean isValid(char c) {
-        return c == 'e' || c == '.' || isDigit(c) || isSign(c);
+        return c == 'e' || isDigit(c) || c == '.' || c == '+' || c == '-';
     }
     
     private boolean isDigit(char c) {
-        return '0' <= c && c <= '9';
-    }
-    
-    private boolean isSign(char c) {
-        return c == '+' || c == '-';
+        return c >= '0' && c <= '9';
     }
 }
