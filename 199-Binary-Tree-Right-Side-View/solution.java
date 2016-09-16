6a78c34770ca51e5b324d6e6a9ac11bc7857d90d
Xu ZHANG <laonawuli@126.com>
2016-09-16 13:41:24 -0700

    [199. Binary Tree Right Side View][Accepted]

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -10,36 +10,18 @@
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        
-        if (root == null) {
-            return list;
-        }
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        Map<TreeNode, Integer> level = new HashMap<>();
-        level.put(root, 1);        
-        int preLevel = 1;
-        int preValue = root.val;
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.remove();
-            int l = level.get(node);
-            if (l != preLevel) {
-                list.add(preValue);
-            }
-            preLevel = l;
-            preValue = node.val;
-            if (node.left != null) {
-                queue.add(node.left);
-                level.put(node.left, l + 1);
-            }
-            if (node.right != null) {
-                queue.add(node.right);
-                level.put(node.right, l + 1);
-            }
-        }
-        list.add(preValue);
-        
+        dfs(list, root, 0);
         return list;
+    }
+    
+    private void dfs(List<Integer> list, TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (list.size() == depth) {
+            list.add(root.val);
+        }
+        dfs(list, root.right, depth + 1);
+        dfs(list, root.left, depth + 1);
     }
 }
