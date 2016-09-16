e268c72be847d66e1dd9eb880d600c66d1c8ae0c
Xu ZHANG <laonawuli@126.com>
2016-09-15 19:45:37 -0700

    [256. Paint House][Accepted]

--- 256-Paint-House/solution.java
+++ 256-Paint-House/solution.java
@@ -3,14 +3,18 @@ public class Solution {
         if (costs == null || costs.length == 0) {
             return 0;
         }
-        int len = costs.length;
         
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < costs.length; i++) {
             costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
             costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
             costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
         }
         
-        return Math.min(Math.min(costs[len - 1][0], costs[len - 1][1]), costs[len - 1][2]);
+        int min = Integer.MAX_VALUE;
+        for (int i : costs[costs.length - 1]) {
+            min = Math.min(min, i);
+        }
+        
+        return min;
     }
 }
