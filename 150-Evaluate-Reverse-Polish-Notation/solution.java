7778b8eb6088245c00ca6cefc02ea5f159dfb136
Xu ZHANG <laonawuli@126.com>
2016-09-17 09:41:10 -0700

    [150. Evaluate Reverse Polish Notation][Accepted]

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -1,29 +1,24 @@
 public class Solution {
     public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
+            return 0;
+        }
         Stack<Integer> stack = new Stack<>();
-        
         for (String str : tokens) {
             if (str.equals("+")) {
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(a + b);
+                stack.push(stack.pop() + stack.pop());
             } else if (str.equals("-")) {
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(b - a);
+                stack.push(-stack.pop() + stack.pop());
             } else if (str.equals("*")) {
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(a * b);
+                stack.push(stack.pop() * stack.pop());
             } else if (str.equals("/")) {
-                int a = stack.pop();
-                int b = stack.pop();            
-                stack.push(b / a);                
+                int n = stack.pop();
+                n = stack.pop() / n;
+                stack.push(n);
             } else {
                 stack.push(Integer.valueOf(str));
             }
         }
-        
         return stack.pop();
     }
 }
