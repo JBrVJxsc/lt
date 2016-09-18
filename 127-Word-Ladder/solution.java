cf6646f16109118933fdb6b1d57e163dafb3f6d3
Xu ZHANG <laonawuli@126.com>
2016-09-18 10:19:56 -0700

    [127. Word Ladder][Accepted]

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,30 +1,30 @@
 public class Solution {
-    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {        
-        Map<String, Integer> map = new HashMap<>();
-        map.put(beginWord, 1);
+    public int ladderLength(String begin, String end, Set<String> words) {
+        if (begin == null || end == null || words == null || words.size() == 0) {
+            return 0;
+        }
+        words.add(end);
         
+        Map<String, Integer> map = new HashMap<>();
         Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
+        queue.add(begin);
+        map.put(begin, 1);
         
         while (!queue.isEmpty()) {
-            String str = queue.remove();
-            
-            for (int i = 0; i < str.length(); i++) {
-                char[] arr = str.toCharArray();
-                for (int j = 0; j < 26; j++) {
-                    arr[i] = (char)('a' + j);                    
-                    String temp = String.valueOf(arr);
-                    
-                    if (temp.equals(endWord)) {
-                        return map.get(str) + 1;
+            String node = queue.remove();
+            int depth = map.get(node);
+            if (node.equals(end)) {
+                return depth;
+            }
+            for (int i = 0; i < node.length(); i++) {
+                char[] chars = node.toCharArray();
+                for (char c = 'a'; c <= 'z'; c++) {
+                    chars[i] = c;
+                    String temp = String.valueOf(chars);
+                    if (map.get(temp) == null && words.contains(temp)) {
+                        queue.add(temp);
+                        map.put(temp, depth + 1);
                     }
-                    
-                    if (!wordList.contains(temp) || map.get(temp) != null) {
-                        continue;
-                    }
-                    
-                    map.put(temp, map.get(str) + 1);
-                    queue.add(temp);
                 }
             }
         }
