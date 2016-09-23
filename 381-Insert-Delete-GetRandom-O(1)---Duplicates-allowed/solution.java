b160090795fcc3a8706a29309101d98b20269339
Xu ZHANG <laonawuli@126.com>
2016-09-22 20:09:37 -0700

    [381. Insert Delete GetRandom O(1) - Duplicates allowed][Accepted]

--- 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
+++ 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
@@ -1,53 +1,54 @@
 public class RandomizedCollection {
 
-    private Map<Integer, Set<Integer>> map = new HashMap<>();
-    private List<Integer> nums = new ArrayList<>();
-    private Random random = new Random();
-    
+    Map<Integer, Set<Integer>> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
+    Random random = new Random();
+
     /** Initialize your data structure here. */
     public RandomizedCollection() {
         
     }
     
-    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
         if (map.get(val) == null) {
             map.put(val, new HashSet<>());
         }
-        map.get(val).add(nums.size());
-        nums.add(val);
-        return map.get(val).size() == 1;
+        list.add(val);
+        Set<Integer> temp = map.get(val);
+        temp.add(list.size() - 1);
+        return temp.size() == 1;
     }
     
-    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if (map.get(val) == null || map.get(val).size() == 0) {
+        Set<Integer> temp = map.get(val);    
+        if (temp == null || temp.size() == 0) {
             return false;
         }
-        Set<Integer> set = map.get(val);
-        int index = set.iterator().next();
-        set.remove(index);
+        int index = temp.iterator().next();
+        temp.remove(index);
         
-        if (index != nums.size() - 1) {
-            int last = nums.get(nums.size() - 1);
-            nums.set(index, last);
-            map.get(last).add(index);
-            map.get(last).remove(nums.size() - 1);
+        if (index != list.size() - 1) {
+            int last = list.get(list.size() - 1);
+            temp = map.get(last);
+            temp.remove(list.size() - 1);
+            temp.add(index);
+            list.set(index, last);
         }
-        
-        nums.remove(nums.size() - 1);
+        list.remove(list.size() - 1);
         return true;
     }
     
-    /** Get a random element from the collection. */
+    /** Get a random element from the set. */
     public int getRandom() {
-        return nums.get(random.nextInt(nums.size()));
+        return list.get(random.nextInt(list.size()));
     }
 }
 
 /**
- * Your RandomizedCollection object will be instantiated and called as such:
- * RandomizedCollection obj = new RandomizedCollection();
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
  * boolean param_1 = obj.insert(val);
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
