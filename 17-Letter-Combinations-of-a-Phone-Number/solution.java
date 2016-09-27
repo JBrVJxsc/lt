5b4ca7ed2b08fddc312048dfdafb933bc3a81dea
Xu ZHANG <laonawuli@126.com>
2016-09-26 21:06:21 -0700

    [17. Letter Combinations of a Phone Number][Accepted]

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,16 +1,14 @@
 public class Solution {
-    String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
     public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.length() == 0) {
-            return new ArrayList<>();
-        }
-        
         List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
         for (char c : digits.toCharArray()) {
             result = combine(result, map[c - '0']);
         }
-        
         return result;
     }
     
@@ -21,13 +19,13 @@ public class Solution {
             }
             return list;
         } else {
-            List<String> temp = new ArrayList<>();
+            List<String> result = new ArrayList<>();
             for (String s : list) {
                 for (char c : str.toCharArray()) {
-                    temp.add(s + String.valueOf(c));
+                    result.add(s + String.valueOf(c));
                 }                
             }
-            return temp;
+            return result;
         }
     }
 }
