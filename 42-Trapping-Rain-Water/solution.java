fd457e3ec934d34b420a1050d350b329359df4df
Xu ZHANG <laonawuli@126.com>
2016-09-27 08:53:56 -0700

    [42. Trapping Rain Water][Accepted]

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -3,27 +3,28 @@ public class Solution {
         if (height == null || height.length == 0) {
             return 0;
         }
-        
-        int[] left = new int[height.length];
-        int[] right = new int[height.length];
+        int[] l = new int[height.length];
+        int[] r = new int[height.length];
         
         int max = height[0];
         for (int i = 1; i < height.length; i++) {
-            left[i] = max;
+            l[i] = max;
             max = Math.max(max, height[i]);
         }
         
         max = height[height.length - 1];
         for (int i = height.length - 2; i >= 0; i--) {
-            right[i] = max;
-            max = Math.max(max, height[i]);
+            r[i] = max;
+            max = Math.max(max, height[i]);            
         }
         
-        int count = 0;        
+        int sum = 0;
         for (int i = 1; i < height.length - 1; i++) {
-            int dif = Math.min(left[i], right[i]) - height[i];
-            count += dif > 0 ? dif : 0;
+            int min = Math.min(l[i], r[i]);
+            if (height[i] < min) {
+                sum += min - height[i];
+            }
         }
-        return count;
+        return sum;
     }
 }
