62ebef887f4ab5986e64febef00ce286a1ac9ce7
Xu ZHANG <laonawuli@126.com>
2016-09-26 20:07:39 -0700

    [200. Number of Islands][Accepted]

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,35 +1,32 @@
 public class Solution {
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        if (row == 0) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
+        int row = grid.length;
         int col = grid[0].length;
-        int counter = 0;
+        int count = 0;
         
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (dfs(grid, i, j, row, col) > 0) {
-                    counter++;
+                    count ++;
                 }
             }
         }
-        
-        return counter;
+        return count;
     }
     
-    private int dfs(char[][] grid, int i, int j, int row, int col) {
-        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1') {
+    private int dfs(char[][] grid, int x, int y, int row, int col) {
+        if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] != '1') {
             return 0;
         }
-        grid[i][j] = '#';
-        
-        int counter = 1;
-        counter += dfs(grid, i - 1, j, row, col);
-        counter += dfs(grid, i + 1, j, row, col);
-        counter += dfs(grid, i, j - 1, row, col);
-        counter += dfs(grid, i, j + 1, row, col);
-        
-        return counter;
+        grid[x][y] = 'x';
+        int num = 1;
+        num += dfs(grid, x + 1, y, row, col);
+        num += dfs(grid, x - 1, y, row, col);
+        num += dfs(grid, x, y + 1, row, col);
+        num += dfs(grid, x, y - 1, row, col);
+        return num;
     }
 }
