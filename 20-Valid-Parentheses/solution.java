77405500370f4548dcc8f804c64105808cf5dab6
Xu ZHANG <laonawuli@126.com>
2016-09-26 21:10:30 -0700

    [20. Valid Parentheses][Accepted]

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -3,23 +3,22 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return true;
         }
-        
         Stack<Character> stack = new Stack<>();
-        
         for (char c : s.toCharArray()) {
             if (stack.isEmpty()) {
                 stack.push(c);
-            } else if (c == ')' && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == ']' && stack.peek() == '[') {
-                stack.pop();
-            } else if (c == '}' && stack.peek() == '{') {
-                stack.pop();
             } else {
-                stack.push(c);
+                if (stack.peek() == '(' && c == ')') {
+                    stack.pop();
+                } else if (stack.peek() == '{' && c == '}') {
+                    stack.pop();
+                } else if (stack.peek() == '[' && c == ']') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
             }
         }
-        
         return stack.isEmpty();
     }
 }
