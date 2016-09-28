806bf08eeeb68f0facfddb45b25ad9458ce493f8
Xu ZHANG <laonawuli@126.com>
2016-09-28 07:29:14 -0700

    [23. Merge k Sorted Lists][Accepted]

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -11,26 +11,25 @@ public class Solution {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        
-        Queue<ListNode> pq = new PriorityQueue<>((a, b) -> {
+        Queue<ListNode> queue = new PriorityQueue<>((a, b) -> {
             return Integer.compare(a.val, b.val);
         });
         
         for (ListNode node : lists) {
             if (node != null) {
-                pq.add(node);
+                queue.add(node);
             }
         }
         
         ListNode dummy = new ListNode(-1);
         ListNode next = dummy;
         
-        while (!pq.isEmpty()) {
-            ListNode node = pq.remove();
+        while (!queue.isEmpty()) {
+            ListNode node = queue.remove();
             next.next = node;
             node = node.next;
             if (node != null) {
-                pq.add(node);
+                queue.add(node);
             }
             next = next.next;
         }
