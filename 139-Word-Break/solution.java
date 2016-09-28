51e556f92a6ae9b5ec0c233d6fe225cf960c5cf5
Xu ZHANG <laonawuli@126.com>
2016-09-27 20:31:39 -0700

    [139. Word Break][Accepted]

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,16 +1,31 @@
 public class Solution {
-    public boolean wordBreak(String s, Set<String> dict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-        
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.contains(s.substring(j, i))) {
-                    dp[i] = true;
+    public boolean wordBreak(String s, Set<String> words) {
+        if (s == null || s.length() == 0 || words == null || words.size() == 0) {
+            return false;
+        }
+        return dfs(s, words, 0, new boolean[s.length()], new boolean[s.length()]);
+    }
+    
+    private boolean dfs(String s, Set<String> words, int index, boolean[] visited, boolean[] result) {
+        if (index == s.length()) {
+            return true;
+        }
+        if (visited[index]) {
+            return result[index];
+        }
+        visited[index] = true;
+        for (int i = index; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                if (words.contains(sub)) {
+                    if (dfs(s, words, j, visited, result)) {
+                        result[i] = true;
+                        break;
+                    }
                 }
             }
         }
         
-        return dp[s.length()];
+        return result[index];
     }
 }
