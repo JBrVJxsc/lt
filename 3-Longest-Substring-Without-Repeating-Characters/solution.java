603d664e2f48294f56ca3715e5c878e6f886b47e
Xu ZHANG <laonawuli@126.com>
2016-09-27 19:02:16 -0700

    [3. Longest Substring Without Repeating Characters][Accepted]

--- 3-Longest-Substring-Without-Repeating-Characters/solution.java
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.java
@@ -3,22 +3,18 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        
-        int max = 1;
-        int start = 0;
         Map<Character, Integer> map = new HashMap<>();
-        map.put(s.charAt(0), 0);
-        
-        for (int i = 1; i < s.length(); i++) {
+        int l = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             Integer index = map.get(c);
-            if (index != null && index >= start) {
-                start = index + 1;
+            if (index != null && index >= l) {
+                l = index + 1;
             }
             map.put(c, i);
-            max = Math.max(max, i - start + 1);
+            max = Math.max(max, i - l + 1);            
         }
-        
         return max;
     }
 }
