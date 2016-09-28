0d87d3a093e6ab71f6511616d22134a71e5778c7
Xu ZHANG <laonawuli@126.com>
2016-09-27 20:40:25 -0700

    [121. Best Time to Buy and Sell Stock][Accepted]

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -1,20 +1,14 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) {
+        if (prices == null || prices.length == 0) {
             return 0;
         }
-        
         int min = prices[0];
         int max = 0;
-        
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] >= min) {
-                max = Math.max(max, prices[i] - min);
-            } else {
-                min = prices[i];
-            }
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        
         return max;
     }
 }
