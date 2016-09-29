9c29a87b9a2df1e2794a4d1cc47b917b302d0ad4
Xu ZHANG <laonawuli@126.com>
2016-09-28 20:17:09 -0700

    [387. First Unique Character in a String][Accepted]

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -1,14 +1,23 @@
 public class Solution {
     public int firstUniqChar(String s) {
-        int[] counter = new int[26];
-        for (char c : s.toCharArray()) {
-            counter[c - 'a']++;
-        }
+        int[] map = new int[26];
+        
         for (int i = 0; i < s.length(); i++) {
-            if (counter[s.charAt(i) - 'a'] == 1) {
-                return i;
+            int pos = s.charAt(i) - 'a';
+            if (map[pos] == 0) {
+                map[pos] = i + 1;
+            } else {
+                map[pos] = -1;
             }
         }
+        
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+            if (map[pos] > 0) {
+                return map[pos] - 1;
+            }
+        }
+        
         return -1;
     }
 }
