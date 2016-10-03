7d81359c0007b41e80f962f4e66d99aafb74c0b6
Xu ZHANG <laonawuli@126.com>
2016-10-02 20:54:13 -0700

    [205. Isomorphic Strings][Accepted]

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -3,30 +3,25 @@ public class Solution {
         if (s == null && t == null) {
             return true;
         }
-        if (s == null || t == null) {
-            return false;
-        }        
-        if (s.length() != t.length()) {
+        if (s == null || t == null || s.length() != t.length()) {
             return false;
         }
         
-        char[] map1 = new char[256];
-        char[] map2 = new char[256];
-        
+        char[] dic1 = new char[256];
+        char[] dic2 = new char[256];
         for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            
-            if (map1[c1] != 0 && map1[c1] != c2) {
+            if (dic1[c1] == 0) {
+                dic1[c1] = c2;
+            } else if (dic1[c1] != c2) {
                 return false;
             }
-            
-            if (map2[c2] != 0 && map2[c2] != c1) {
+            if (dic2[c2] == 0) {
+                dic2[c2] = c1;
+            } else if (dic2[c2] != c1) {
                 return false;
             }            
-            
-            map1[c1] = c2;
-            map2[c2] = c1;
         }
         
         return true;
