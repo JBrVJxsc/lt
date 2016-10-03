720f3c74fd70a6459114f831e8cb9a79ef53f1ec
Xu ZHANG <laonawuli@126.com>
2016-10-02 20:21:07 -0700

    [245. Shortest Word Distance III][Accepted]

--- 245-Shortest-Word-Distance-III/solution.java
+++ 245-Shortest-Word-Distance-III/solution.java
@@ -1,30 +1,30 @@
 public class Solution {
-    public int shortestWordDistance(String[] words, String word1, String word2) {
+    public int shortestWordDistance(String[] words, String w1, String w2) {
         int min = Integer.MAX_VALUE;
-        if (word1.equals(word2)) {
-            int pre = -1;
+        if (w1.equals(w2)) {
+            int p = -1;
             for (int i = 0; i < words.length; i++) {
-                if (words[i].equals(word1)) {
-                    if (pre == -1) {
-                        pre = i;
+                if (words[i].equals(w1)) {
+                    if (p == -1) {
+                        p = i;
                     } else {
-                        min = Math.min(min, i - pre);
-                        pre = i;
+                        min = Math.min(min, i - p);
+                        p = i;
                     }
                 }
             }
         } else {
-            int i1 = -1;
-            int i2 = -1;
+            int p1 = -1;
+            int p2 = -1;
             for (int i = 0; i < words.length; i++) {
-                if (words[i].equals(word1)) {
-                    i1 = i;
+                if (words[i].equals(w1)) {
+                    p1 = i;
                 }
-                if (words[i].equals(word2)) {
-                    i2 = i;
-                }
-                if (i1 != -1 && i2 != -1) {
-                    min = Math.min(min, Math.abs(i1 - i2));
+                if (words[i].equals(w2)) {
+                    p2 = i;
+                }     
+                if (p1 != -1 && p2 != -1) {
+                    min = Math.min(min, Math.abs(p1 - p2));
                 }
             }
         }
