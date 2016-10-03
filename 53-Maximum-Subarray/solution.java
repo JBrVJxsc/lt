994d74663b2e4983698d833d1b8c402ce34ddb8f
Xu ZHANG <laonawuli@126.com>
2016-10-02 20:26:06 -0700

    [53. Maximum Subarray][Accepted]

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -3,12 +3,15 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        
-        int max = nums[0];
         int sum = nums[0];
+        int max = nums[0];
         
         for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(nums[i], nums[i] + sum);
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
             max = Math.max(max, sum);
         }
         
