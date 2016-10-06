4f1d335f9045f33983dc3b154efc78115e4259e8
Xu ZHANG <laonawuli@126.com>
2016-10-06 10:25:19 -0700

    [68. Text Justification][Accepted]

--- 68-Text-Justification/solution.java
+++ 68-Text-Justification/solution.java
@@ -1,61 +1,58 @@
 public class Solution {
     public List<String> fullJustify(String[] words, int max) {
-        List<String> list = new ArrayList<>();
-        if (words == null || words.length == 0) {
-            return list;
-        }
-        
+        List<String> result = new ArrayList<>();
         List<String> cur = new ArrayList<>();
         int curLen = 0;
+        
         for (int i = 0; i < words.length; i++) {
-            String str = words[i];
-            if (curLen + str.length() + cur.size() > max) {
-                str = justify(cur, curLen, max);
-                list.add(str);
+            if (curLen + words[i].length() + cur.size() <= max) {
+                cur.add(words[i]);
+                curLen += words[i].length();
+            } else {
+                result.add(justify(cur, curLen, max));
                 cur = new ArrayList<>();
                 curLen = 0;
                 i--;
-            } else {
-                cur.add(str);
-                curLen += str.length();
             }
         }
         
-        String str = justifyLast(cur, curLen, max);
-        list.add(str);
-        return list;
+        result.add(justifyLast(cur, max));
+        return result;
     }
     
     private String justify(List<String> list, int len, int max) {
-        int remain = max - len;
         if (list.size() == 1) {
-            return padRight(list.get(0), remain);
+            return padRight(list.get(0), max);
         } else {
+            int remain = max - len;
             while (remain > 0) {
-                for (int i = 0; i < list.size() - 1 && remain-- > 0; i++) {
-                    list.set(i, list.get(i) + " ");
+                for (int i = 0; i < list.size() - 1 && remain > 0; i++) {
+                    String str = list.get(i) + " ";
+                    list.set(i, str);
+                    remain--;
                 }
             }
             return merge(list);
         }
     }
     
-    private String justifyLast(List<String> list, int len, int max) {
-        int remain = max - len;
-        for (int i = 0; i < list.size() - 1; i++) {
-            list.set(i, list.get(i) + " ");
-            remain--;
+    private String justifyLast(List<String> list, int max) {
+        String str = list.get(0);
+        if (list.size() > 1) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                str = list.get(i) + " ";
+                list.set(i, str);
+            }
+            str = merge(list);
         }
-        list.set(list.size() - 1, padRight(list.get(list.size() - 1), remain));
-        return merge(list);
+        return padRight(str, max);
     }
     
-    private String padRight(String str, int num) {
-        StringBuilder sb = new StringBuilder(str);
-        while (num-- > 0) {
-            sb.append(" ");
+    private String padRight(String str, int len) {
+        while (len != str.length()) {
+            str += " ";
         }
-        return sb.toString();
+        return str;
     }
     
     private String merge(List<String> list) {
