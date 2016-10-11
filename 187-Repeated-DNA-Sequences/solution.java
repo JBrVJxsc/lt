d1ece912c28150d89b955d29c236a184d5d7a8e8
Xu ZHANG <laonawuli@126.com>
2016-10-11 14:32:43 -0700

    [187. Repeated DNA Sequences][Accepted]

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,15 +1,18 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> list = new ArrayList<>();
+        
         if (s == null || s.length() < 11) {
             return list;
         }
         
-        int num = 0;
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> all = new HashSet<>();
         Set<Integer> added = new HashSet<>();
+        int num = 0;
+        
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            
             int n = 0;
             if (c == 'C') {
                 n = 1;
@@ -19,21 +22,20 @@ public class Solution {
                 n = 3;
             }
             
-            if (i < 9) {
-                num = num << 2;
-                num += n;
-            } else {
-                num = num << 2;
-                num += n;                
-                num &= (1 << 20) - 1;
-                if (set.contains(num) && !added.contains(num)) {
-                    list.add(s.substring(i - 9, i + 1));
+            num <<= 2;
+            num += n;
+            num &= (1 << 20) - 1;
+            
+            if (i > 8) {              
+                if (!all.contains(num)) {
+                    all.add(num);
+                } else if (!added.contains(num)) {
                     added.add(num);
-                } else {
-                    set.add(num);
+                    list.add(s.substring(i - 9, i + 1));                        
                 }
             }
         }
+        
         return list;
     }
 }
