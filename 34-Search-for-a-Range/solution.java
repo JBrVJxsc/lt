2899bdd339058b4483643a94e5dbef8c0c9d6829
Xu ZHANG <laonawuli@126.com>
2016-10-11 08:35:18 -0700

    [34. Search for a Range][Accepted]

--- 34-Search-for-a-Range/solution.java
+++ 34-Search-for-a-Range/solution.java
@@ -1,41 +1,48 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{left(nums, target, 0, nums.length - 1), right(nums, target, 0, nums.length - 1)};
-    }
-    
-    private int left(int[] nums, int target, int l, int r) {
-        if (l > r) {
-            return -1;
-        }
-        int m = l + (r - l) / 2;
-        if (nums[m] == target) {
-            if (m == 0 || nums[m - 1] != target) {
-                return m;
-            } else {
-                return left(nums, target, l, m - 1);
+        int r1 = -1;
+        int r2 = -1;
+        
+        if (nums != null && nums.length != 0) {
+            int l = 0;
+            int r = nums.length - 1;
+            
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                if (nums[m] > target) {
+                    r = m - 1;
+                } else if (nums[m] < target) {
+                    l = m + 1;
+                } else {
+                    if (m == 0 || nums[m - 1] != target) {
+                        r1 = m;
+                        break;
+                    } else {
+                        r = m - 1;
+                    }
+                }
             }
-        } else if (nums[m] > target) {
-            return left(nums, target, l, m - 1);
-        } else {
-            return left(nums, target, m + 1, r);
+            
+            l = 0;
+            r = nums.length - 1;
+            
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                if (nums[m] > target) {
+                    r = m - 1;
+                } else if (nums[m] < target) {
+                    l = m + 1;
+                } else {
+                    if (m == nums.length - 1 || nums[m + 1] != target) {
+                        r2 = m;
+                        break;
+                    } else {
+                        l = m + 1;
+                    }
+                }
+            }            
         }
+        
+        return new int[]{r1, r2};
     }
-    
-    private int right(int[] nums, int target, int l, int r) {
-        if (l > r) {
-            return -1;
-        }        
-        int m = l + (r - l) / 2;
-        if (nums[m] == target) {
-            if (m == nums.length - 1 || nums[m + 1] != target) {
-                return m;
-            } else {
-                return right(nums, target, m + 1, r);
-            }
-        } else if (nums[m] > target) {
-            return right(nums, target, l, m - 1);
-        } else {
-            return right(nums, target, m + 1, r);
-        }        
-    }    
 }
