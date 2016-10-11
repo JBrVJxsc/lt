9be4e83f3f750f30be90f94494534710c6d5807a
Xu ZHANG <laonawuli@126.com>
2016-10-10 20:59:45 -0700

    [65. Valid Number][Accepted]

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -9,57 +9,77 @@ public class Solution {
             return false;
         }
         
-        s = s.toLowerCase();
-        
-        int dotNum = 0;
-        int digitNum = 0;
-        int eNum = 0;
-        int signNum = 0;
+        int numOfDot = 0;
+        int numOfDigit = 0;
+        int numOfE = 0;
+        int numOfSign = 0;
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            if ('0' <= c && c <= '9') {
-                digitNum++;
-            } else if (c == '.') {
-                if (dotNum == 1) {
-                    return false;
-                }
-                if (i == s.length() - 1 && digitNum == 0) {
-                    return false;
-                }
-                if (eNum != 0) {
-                    return false;
-                }
-                dotNum++;
-            }
-            else if (c == 'e') {
-                if (eNum == 1) {
-                    return false;
-                }
-                if (i == s.length() - 1) {
-                    return false;
-                }
-                if (digitNum == 0) {
-                    return false;
-                }
-                eNum++;
-            } else if (c == '+' || c == '-') {
-                if (signNum == 2) {
-                    return false;
-                }
-                if (i == s.length() - 1) {
-                    return false;
-                }
-                if (i != 0 && eNum == 0) {
-                    return false;
-                }
-                signNum++;
-            } else {
+            if (!isValid(c)) {
                 return false;
+            }
+            if (isDigit(c)) {
+                numOfDigit++;
+            }
+            if (isDot(c)) {
+                numOfDot++;
+                if (numOfDot == 2) {
+                    return false;
+                }
+                if (i == s.length() - 1 && numOfDigit == 0) {
+                    return false;
+                }
+                if (numOfE > 0) {
+                    return false;
+                }
+            }
+            if (isE(c)) {
+                numOfE++;
+                if (numOfE == 2) {
+                    return false;
+                }
+                if (i == s.length() - 1) {
+                    return false;
+                }
+                if (numOfDigit == 0) {
+                    return false;
+                }
+            }
+            if (isSign(c)) {
+                numOfSign++;
+                if (numOfSign == 3) {
+                    return false;
+                }
+                if (i == s.length() - 1) {
+                    return false;
+                }
+                if (i != 0 && numOfE == 0) {
+                    return false;
+                }
             }
         }
         
         return true;
+    }
+    
+    private boolean isValid(char c) {
+        return isDigit(c) || isE(c) || isSign(c) || isDot(c);
+    }
+    
+    private boolean isE(char c) {
+        return c == 'e' || c == 'E';
+    }
+    
+    private boolean isDigit(char c) {
+        return '0' <= c && c <= '9';
+    }
+    
+    private boolean isSign(char c) {
+        return c == '-' || c == '+';
+    }
+    
+    private boolean isDot(char c) {
+        return c == '.';
     }
 }
