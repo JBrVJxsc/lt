57666bc4568c5cd94be3096b40477b4fd59aef55
Xu ZHANG <laonawuli@126.com>
2016-10-11 20:18:33 -0700

    [152. Maximum Product Subarray][Accepted]

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -10,12 +10,12 @@ public class Solution {
         
         for (int i = 1; i < nums.length; i++) {
             int curMin = nums[i];
-            curMin = Math.min(curMin, nums[i] * preMin);
             curMin = Math.min(curMin, nums[i] * preMax);
+            curMin = Math.min(curMin, nums[i] * preMin);
             
             int curMax = nums[i];
-            curMax = Math.max(curMax, nums[i] * preMin);
             curMax = Math.max(curMax, nums[i] * preMax);
+            curMax = Math.max(curMax, nums[i] * preMin);
             
             max = Math.max(max, curMax);
             preMin = curMin;
