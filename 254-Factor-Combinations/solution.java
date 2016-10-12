511fb30bc2c7f2e6832cf5eba8f09e26a8717fb7
Xu ZHANG <laonawuli@126.com>
2016-10-11 20:36:12 -0700

    [254. Factor Combinations][Accepted]

--- 254-Factor-Combinations/solution.java
+++ 254-Factor-Combinations/solution.java
@@ -1,42 +1,24 @@
 public class Solution {
     public List<List<Integer>> getFactors(int n) {
         List<List<Integer>> lists = new ArrayList<>();
-        if (n < 4) {
-            return lists;
-        }
-        
-        List<Integer> factors = factors(n);
-        dfs(lists, factors, new ArrayList<>(), 1, n, 0);
+        dfs(lists, new ArrayList<>(), n, 2);
         return lists;
     }
- 
-    private void dfs(List<List<Integer>> lists, List<Integer> factors, List<Integer> cur, int curProd, int n, int index) {
-        if (curProd == n) {
-            lists.add(new ArrayList<>(cur));
+    
+    private void dfs(List<List<Integer>> lists, List<Integer> cur, int n, int start) {
+        if (n == 1) {
+            if (cur.size() > 1) {
+                lists.add(new ArrayList<>(cur));
+            }
             return;
         }
         
-        for (int i = index; i < factors.size(); i++) {
-            int num = factors.get(i);
-            if (curProd > n / num) {
-                return;
-            }
-            curProd *= num;
-            cur.add(num);
-            dfs(lists, factors, cur, curProd, n, i);
-            curProd /= num;
-            cur.remove(cur.size() - 1);
-            
-        }
-    }
-    
-    private List<Integer> factors(int n) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 2; i < n; i++) {
+        for (int i = start; i <= n; i++) {
             if (n % i == 0) {
-                list.add(i);
+                cur.add(i);
+                dfs(lists, cur, n / i, i);
+                cur.remove(cur.size() - 1);
             }
         }
-        return list;
     }
 }
